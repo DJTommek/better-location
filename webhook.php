fef4e0480863d718d4b39dee21ae1aec37f54671
DJTommek <tomas@palider.cz>
2020-07-13 00:15:39 +0200

    Added logging all webhook requests

--- webhook.php
+++ webhook.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/src/config.php';
 
-$updateData = json_decode(file_get_contents('php://input'), true);
+$input = file_get_contents('php://input');
+$updateData = json_decode($input, true);
+\Tracy\Debugger::log('TG Input: ' . $input, \Tracy\ILogger::DEBUG);
+
 $telegramCustomWrapper = new \TelegramCustomWrapper\TelegramCustomWrapper(TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_NAME);
 $telegramCustomWrapper->handleUpdate($updateData);
+echo 'ok';
