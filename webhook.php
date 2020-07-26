7c74d33fb827581d4149cb0c18bc2cb42b896e5a
DJTommek <tomas@palider.cz>
2020-07-26 23:33:08 +0200

    Extended catching errors on webhook page

--- webhook.php
+++ webhook.php
@@ -7,11 +7,11 @@ require_once __DIR__ . '/src/config.php';
 
 printf('<p>Go back to <a href="./index.php">index.php</a></p>');
 
-$input = file_get_contents('php://input');
-$updateData = json_decode($input, true, 512, JSON_THROW_ON_ERROR);
-DummyLogger::log(DummyLogger::NAME_TELEGRAM_INPUT, $updateData);
-
 try {
+	$input = file_get_contents('php://input');
+	$updateData = json_decode($input, true, 512, JSON_THROW_ON_ERROR);
+	DummyLogger::log(DummyLogger::NAME_TELEGRAM_INPUT, $updateData);
+
 	$telegramCustomWrapper = new \TelegramCustomWrapper\TelegramCustomWrapper(TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_NAME);
 	$telegramCustomWrapper->handleUpdate($updateData);
 	printf('OK.');
