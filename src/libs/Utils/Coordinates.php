4cef87a58ac43ea3cced55d421e5fa4356535929
DJTommek <tomas@palider.cz>
2020-07-26 17:18:11 +0200

    Added Coordinates::distance()

--- src/libs/Utils/Coordinates.php
+++ src/libs/Utils/Coordinates.php
@@ -11,6 +11,8 @@ class Coordinates
 	const EAST = 'E';
 	const WEST = 'W';
 
+	const EARTH_RADIUS = 6371000; // in meters
+
 	// @TODO maybe use universal regex for various of formats?
 	// https://regex101.com/r/gD9aU5/1
 
@@ -114,4 +116,31 @@ class Coordinates
 			throw new \InvalidArgumentException('Invalid hemisphere');
 		}
 	}
+
+	/**
+	 * Calculates the great-circle distance between two points, with the Vincenty formula.
+	 *
+	 * @param float $latitudeFrom Latitude of start point in [deg decimal]
+	 * @param float $longitudeFrom Longitude of start point in [deg decimal]
+	 * @param float $latitudeTo Latitude of target point in [deg decimal]
+	 * @param float $longitudeTo Longitude of target point in [deg decimal]
+	 * @param float $earthRadius Mean earth radius in [m]
+	 * @return float Distance between points in [m] (same as earthRadius)
+	 * @author https://stackoverflow.com/a/10054282/3334403
+	 */
+	public static function distance(float $latitudeFrom, float $longitudeFrom, float $latitudeTo, float $longitudeTo, float $earthRadius = self::EARTH_RADIUS) {
+		// convert from degrees to radians
+		$latFrom = deg2rad($latitudeFrom);
+		$lonFrom = deg2rad($longitudeFrom);
+		$latTo = deg2rad($latitudeTo);
+		$lonTo = deg2rad($longitudeTo);
+
+		$lonDelta = $lonTo - $lonFrom;
+		$a = pow(cos($latTo) * sin($lonDelta), 2) + pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+		$b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+		$angle = atan2(sqrt($a), $b);
+		return $angle * $earthRadius;
+	}
+
 }
