36051de11d93307f0a8002c5eb846c652cc4ff7e
DJTommek <tomas@palider.cz>
2020-08-09 16:49:15 +0200

    Changed charset to utf8mb4 to support icons

--- asset/sql/structure.sql
+++ asset/sql/structure.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `better_location` DEFAULT CHARACTER SET utf8 COLLATE utf8mb4_bin;
+CREATE DATABASE `better_location` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 USE `better_location`;
 
 CREATE TABLE `better_location_user` (
@@ -9,7 +9,7 @@ CREATE TABLE `better_location_user` (
   `user_last_update` datetime NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_telegram_id` (`user_telegram_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `better_location_chat` (
   `chat_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,4 +20,4 @@ CREATE TABLE `better_location_chat` (
   `chat_last_update` datetime NOT NULL,
   PRIMARY KEY (`chat_id`),
   UNIQUE KEY `chat_telegram_id` (`chat_telegram_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
