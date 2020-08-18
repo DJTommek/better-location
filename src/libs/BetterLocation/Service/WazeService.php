0d6b5c61500ff0e442614bacc9afb89f150d2e8e
DJTommek <tomas@palider.cz>
2020-08-18 19:31:11 +0200

    Waze Service: added support for http version of links, added support for links without www, added tests

--- src/libs/BetterLocation/Service/WazeService.php
+++ src/libs/BetterLocation/Service/WazeService.php
@@ -66,21 +66,11 @@ final class WazeService extends AbstractService
 	public static function isShortUrl(string $url): bool {
 		// Mapy.cz short link:
 		// https://waze.com/ul/hu2fk8zezt
-		return !!(preg_match('/https:\/\/waze\.com\/ul\/[a-z0-9A-Z]+$/', $url));
+		return !!(preg_match('/^https?:\/\/waze\.com\/ul\/[a-z0-9A-Z]+$/', $url));
 	}
 
 	public static function isNormalUrl(string $url): bool {
-		// https://www.waze.com/ul?ll=50.06300713%2C14.43964005&navigate=yes&zoom=15
-		// https://www.waze.com/ul?ll=49.87707960%2C18.43036300&navigate=yes
-		// https://www.waze.com/ul?ll=50.06300713%2C14.43964005
-		//
-		// https://www.waze.com/cs/livemap/directions?latlng=50.063007132127616%2C14.439640045166016&utm_campaign=waze_website&utm_expid=.K6QI8s_pTz6FfRdYRPpI3A.0&utm_referrer=https%3A%2F%2Fwww.waze.com%2Fcs%2Faccount&utm_source=waze_website
-		// https://www.waze.com/cs/livemap/directions?latlng=50.063007132127616%2C14.439640045166016
-		//
-		// https://www.waze.com/cs/livemap/directions?utm_expid=.K6QI8s_pTz6FfRdYRPpI3A.0&utm_referrer=&to=ll.50.07734439%2C14.43475842
-		// https://www.waze.com/cs/livemap/directions?to=ll.50.07734439%2C14.43475842
-		// https://www.waze.com/cs/livemap/directions?to=ll.49.8770796%2C18.430363
-		return substr($url, 0, mb_strlen(self::LINK)) === self::LINK;
+		return !!(preg_match('/^https?:\/\/(?:www\.)?waze\.com\/.+/', $url));
 	}
 
 	/**
