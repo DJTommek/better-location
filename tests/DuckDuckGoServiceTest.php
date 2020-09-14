b449fe87cfcb4f2fdc3975f7c4415448f2545e02
DJTommek <tomas@palider.cz>
2020-09-14 21:07:47 +0200

    DuckDuckGoService: added test for links generating

---
+++ tests/DuckDuckGoServiceTest.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+use BetterLocation\Service\DuckDuckGoService;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../src/config.php';
+
+
+final class DuckDuckGoServiceTest extends TestCase
+{
+	public function testGenerateShareLink(): void {
+		$this->assertEquals('https://duckduckgo.com/?q=50.087451,14.420671&iaxm=maps', DuckDuckGoService::getLink(50.087451, 14.420671));
+		$this->assertEquals('https://duckduckgo.com/?q=50.100000,14.500000&iaxm=maps', DuckDuckGoService::getLink(50.1, 14.5));
+		$this->assertEquals('https://duckduckgo.com/?q=-50.200000,14.600000&iaxm=maps', DuckDuckGoService::getLink(-50.2, 14.6000001)); // round down
+		$this->assertEquals('https://duckduckgo.com/?q=50.300000,-14.700001&iaxm=maps', DuckDuckGoService::getLink(50.3, -14.7000009)); // round up
+		$this->assertEquals('https://duckduckgo.com/?q=-50.400000,-14.800008&iaxm=maps', DuckDuckGoService::getLink(-50.4, -14.800008));
+	}
+}
