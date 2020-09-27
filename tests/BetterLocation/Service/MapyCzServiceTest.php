27681111fdb0821a78c6b6899a829760ce172409
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-09-27 15:48:31 +0200

    Fixed MapyCzServiceTest (changed position of bus stop)

--- tests/BetterLocation/Service/MapyCzServiceTest.php
+++ tests/BetterLocation/Service/MapyCzServiceTest.php
@@ -125,7 +125,7 @@ final class MapyCzServiceTest extends TestCase
 			$this->assertEquals('49.444980,11.109055', MapyCzService::parseCoords('https://en.mapy.cz/zakladni?x=11.0924687&y=49.4448356&z=15&source=osm&id=112448327')->__toString());
 			// negative coordinates (source OSM)
 			$this->assertEquals('54.766918,-101.873729', MapyCzService::parseCoords('https://en.mapy.cz/zakladni?x=-101.8754373&y=54.7693842&z=15&source=osm&id=1000536418')->__toString());
-			$this->assertEquals('-18.917167,47.535756', MapyCzService::parseCoords('https://en.mapy.cz/zakladni?x=47.5323757&y=-18.9155159&z=16&source=osm&id=1040985945')->__toString());
+			$this->assertEquals('-18.917187,47.535795', MapyCzService::parseCoords('https://en.mapy.cz/zakladni?x=47.5323757&y=-18.9155159&z=16&source=osm&id=1040985945')->__toString());
 			$this->assertEquals('-45.870330,-67.507560', MapyCzService::parseCoords('https://en.mapy.cz/zakladni?x=-67.5159386&y=-45.8711989&z=15&source=osm&id=17164289')->__toString());
 		}
 	}
@@ -147,7 +147,8 @@ final class MapyCzServiceTest extends TestCase
 			$this->assertEquals('49.444980,11.109055', MapyCzService::parseCoords('https://en.mapy.cz/s/hozogeruvo')->__toString());
 			// negative coordinates (source OSM)
 			$this->assertEquals('54.766918,-101.873729', MapyCzService::parseCoords('https://en.mapy.cz/s/dasorekeja')->__toString());
-			$this->assertEquals('-18.917167,47.535756', MapyCzService::parseCoords('https://en.mapy.cz/s/maposedeso')->__toString());
+//			$this->assertEquals('-18.917167,47.535756', MapyCzService::parseCoords('https://en.mapy.cz/s/maposedeso')->__toString());
+			$this->assertEquals('-18.917187,47.535795', MapyCzService::parseCoords('https://en.mapy.cz/s/maposedeso')->__toString());
 			$this->assertEquals('-45.870330,-67.507560', MapyCzService::parseCoords('https://en.mapy.cz/s/robelevuja')->__toString());
 		}
 	}
