322bfba44ed6d8750c8f6129342b1a9ab1b933e8
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-10-03 10:38:51 +0200

    Added support for larger Telegram group and user names

--- asset/sql/structure.sql
+++ asset/sql/structure.sql
@@ -4,7 +4,7 @@ USE `better_location`;
 CREATE TABLE `better_location_user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_telegram_id` int(11) NOT NULL,
-  `user_telegram_name` varchar(45) COLLATE utf8mb4_bin DEFAULT NULL,
+  `user_telegram_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   `user_registered` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_last_update` datetime NOT NULL,
   `user_location_lat` DOUBLE(10,6) NULL,
@@ -18,7 +18,7 @@ CREATE TABLE `better_location_chat` (
   `chat_id` int(11) NOT NULL AUTO_INCREMENT,
   `chat_telegram_id` bigint(8) NOT NULL,
   `chat_telegram_type` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `chat_telegram_name` varchar(45) COLLATE utf8mb4_bin DEFAULT NULL,
+  `chat_telegram_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   `chat_registered` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `chat_last_update` datetime NOT NULL,
   PRIMARY KEY (`chat_id`),
