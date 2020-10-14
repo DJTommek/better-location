200cf9cd86a5d16ec1da32fe30257e680e0d0e7d
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-10-14 19:04:07 +0200

    GlympseAPI: Throw Exception for setting dynamic property in Type

--- src/libs/GlympseApi/Types/Type.php
+++ src/libs/GlympseApi/Types/Type.php
@@ -4,4 +4,7 @@ namespace GlympseApi\Types;
 
 abstract class Type
 {
+	public function __set($name, $value) {
+		throw new \OutOfBoundsException(sprintf('Property "%s$%s" is not predefined.', static::class, $name));
+	}
 }
