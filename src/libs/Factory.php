1008ff6422458353627677b8c0af84d6c637a94b
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-10-28 22:27:45 +0100

    GlympseApi: replaced with djtommek/glympse-api library

--- src/libs/Factory.php
+++ src/libs/Factory.php
@@ -28,10 +28,10 @@ class Factory
 		return self::$objects['w3w'];
 	}
 
-	static function Glympse(): \GlympseApi\Glympse
+	static function Glympse(): \DJTommek\GlympseApi\GlympseApi
 	{
 		if (!isset(self::$objects['glympse'])) {
-			self::$objects['glympse'] = new GlympseApi\Glympse(\Config::GLYMPSE_API_USERNAME, \Config::GLYMPSE_API_PASSWORD, \Config::GLYMPSE_API_KEY);
+			self::$objects['glympse'] = new \DJTommek\GlympseApi\GlympseApi(\Config::GLYMPSE_API_USERNAME, \Config::GLYMPSE_API_PASSWORD, \Config::GLYMPSE_API_KEY);
 		}
 		return self::$objects['glympse'];
 	}
