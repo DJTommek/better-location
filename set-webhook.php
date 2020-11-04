91f5bbd0553a9e3944aa97a6623aabf3b719b927
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-11-04 22:19:27 +0100

    Dashboard: setWebhook promise replaced with await

--- set-webhook.php
+++ set-webhook.php
@@ -3,8 +3,10 @@
 use App\Config;
 use Tracy\Debugger;
 use Tracy\ILogger;
+use unreal4u\TelegramAPI\Exceptions\ClientException;
 use unreal4u\TelegramAPI\HttpClientRequestHandler;
 use unreal4u\TelegramAPI\TgLog;
+use function Clue\React\Block\await;
 
 require_once __DIR__ . '/src/bootstrap.php';
 
@@ -16,19 +18,13 @@ if (\App\Dashboard\Status::isTGWebhookUrSet()) {
 
 	$setWebhook = new \unreal4u\TelegramAPI\Telegram\Methods\SetWebhook();
 	$setWebhook->url = Config::TELEGRAM_WEBHOOK_URL;
-
-	$promise = $tgLog->performApiRequest($setWebhook);
-
-	$promise->then(
-		function () {
-			printf('<h1>Success</h1><p>Telegram webhook URL successfully set to <a href="%1$s" target="_blank">%1$s</a></p>.', Config::TELEGRAM_WEBHOOK_URL);
-		},
-		function (\Exception $exception) {
-			printf('<h1>Error</h1><p>Failed to set Telegram webhook URL to <a href="%1$s" target="_blank">%1$s</a>:<br><b>%2$s</b></p>.', Config::TELEGRAM_WEBHOOK_URL, $exception->getMessage());
-			Debugger::log($exception, ILogger::EXCEPTION);
-		}
-	);
-	$loop->run();
+	try {
+		await($tgLog->performApiRequest($setWebhook), $loop);
+		printf('<h1>Success</h1><p>Telegram webhook URL successfully set to <a href="%1$s" target="_blank">%1$s</a></p>.', Config::TELEGRAM_WEBHOOK_URL);
+	} catch (ClientException $exception) {
+		printf('<h1>Error</h1><p>Failed to set Telegram webhook URL to <a href="%1$s" target="_blank">%1$s</a>:<br><b>%2$s</b></p>.', Config::TELEGRAM_WEBHOOK_URL, $exception->getMessage());
+		Debugger::log($exception, ILogger::EXCEPTION);
+	}
 } else {
 	printf('Updating Telegram webhook URL is not allowed. Set "TELEGRAM_WEBHOOK_URL" to some URL and try again.');
 }
