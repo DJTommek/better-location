ac973e4cb85be4253e6176f67cb6b1c855b325a8
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-11-10 22:53:38 +0100

    Dashboard: added info text if no Tracy logs are available

--- index.php
+++ index.php
@@ -145,13 +145,17 @@ if (isset($_GET['delete-tracy-email-sent'])) {
 			<h2>Tracy logs</h2>
 			<?php
 			$tracyLogs = \App\Dashboard\Logs::getTracyLogs(10);
-			foreach ($tracyLogs as $tracyLogName => $tracyLogContent) {
-				$className = $tracyLogName;
-				if (in_array($tracyLogName, [\Tracy\ILogger::ERROR, \Tracy\ILogger::EXCEPTION, \Tracy\ILogger::CRITICAL], true)) {
-					$className = 'danger';
+			if (count($tracyLogs) === 0) {
+				printf('<p>No logs are available.</p>');
+			} else {
+				foreach ($tracyLogs as $tracyLogName => $tracyLogContent) {
+					$className = $tracyLogName;
+					if (in_array($tracyLogName, [\Tracy\ILogger::ERROR, \Tracy\ILogger::EXCEPTION, \Tracy\ILogger::CRITICAL], true)) {
+						$className = 'danger';
+					}
+					printf('<h4 class="text-%s">%s</h4>', $className, $tracyLogName);
+					printf('<pre>%s</pre>', htmlentities(join(PHP_EOL, $tracyLogContent)));
 				}
-				printf('<h4 class="text-%s">%s</h4>', $className, $tracyLogName);
-				printf('<pre>%s</pre>', htmlentities(join(PHP_EOL, $tracyLogContent)));
 			}
 			?>
 			<h2>Dummy logs</h2>
