e6b23786dd6a30b69bfe3eaccde7ed9ecf414260
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-03-07 12:36:02 +0100

    #46 Services refactoring: converted WhatThreeWord service and added more tests

--- src/libs/BetterLocation/Service/WhatThreeWordService.php
+++ src/libs/BetterLocation/Service/WhatThreeWordService.php
@@ -3,23 +3,16 @@
 namespace App\BetterLocation\Service;
 
 use App\BetterLocation\BetterLocation;
-use App\BetterLocation\BetterLocationCollection;
-use App\BetterLocation\Service\Exceptions\InvalidApiKeyException;
-use App\BetterLocation\Service\Exceptions\InvalidLocationException;
-use App\BetterLocation\Service\Exceptions\NotImplementedException;
 use App\BetterLocation\Service\Exceptions\NotSupportedException;
-use App\Factory;
+use App\WhatThreeWord\Helper;
+use Nette\Utils\Arrays;
 
-final class WhatThreeWordService extends AbstractService
+final class WhatThreeWordService extends AbstractServiceNew
 {
 	const NAME = 'W3W';
 
 	const LINK = 'https://what3words.com/';
 	const LINK_SHORT = 'https://w3w.co/';
-
-	// https://developer.what3words.com/tutorial/detecting-if-text-is-in-the-format-of-a-3-word-address/
-	const RE = '/^\/{3}(?:\p{L}\p{M}*){1,}[・.。](?:\p{L}\p{M}*){1,}[・.。](?:\p{L}\p{M}*){1,}$/u';
-	const RE_IN_STRING = '/\/{3}((?:\p{L}\p{M}*){1,}[・.。](?:\p{L}\p{M}*){1,}[・.。](?:\p{L}\p{M}*){1,})/u';
 
 	/** @throws NotSupportedException|\Exception */
 	public static function getLink(float $lat, float $lon, bool $drive = false): string
@@ -27,92 +20,53 @@ final class WhatThreeWordService extends AbstractService
 		if ($drive) {
 			throw new NotSupportedException('Drive link is not supported.');
 		} else {
-			$w3wApi = Factory::WhatThreeWords();
-			// @TODO dirty hack to get stdclass instead of associated array
-			$response = $w3wApi->convertTo3wa($lat, $lon);
-			$error = $w3wApi->getError();
-			if ($error) {
-				// @TODO dirty hack to get stdclass instead of associated array
-				$error = json_decode(json_encode($error));
-				throw new \Exception(sprintf('Unable to get W3W from coordinates: %s', $error->message));
-			}
-			$data = json_decode(json_encode($response));
+			$data = Helper::coordsToWords($lat, $lon);
 			return $data->map;
 		}
 	}
 
-	public static function isValid(string $input): bool
+	public function isValid(): bool
 	{
-		return self::isWords($input) || self::isShortUrl($input) || self::isNormalUrl($input);
+		return $this->isWords() || $this->isUrl();
+	}
+
+	public function process(): void
+	{
+		$data = Helper::wordsToCoords($this->data->words);
+		$betterLocation = new BetterLocation($this->input, $data->coordinates->lat, $data->coordinates->lng, self::class);
+		$betterLocation->setPrefixMessage(sprintf('<a href="%s">%s</a>: <code>%s</code>', $data->map, self::NAME, $data->words));
+		$this->collection->add($betterLocation);
 	}
 
 	/**
-	 * @param string $input words or URL
-	 * @throws \Exception
+	 * @example chladná.naopak.vložit
+	 * @example ///chladná.naopak.vložit
 	 */
-	public static function parseCoords(string $input): BetterLocation
+	private function isWords(): bool
 	{
-		$words = null;
-		if (self::isNormalUrl($input)) {
-			$words = str_replace(self::LINK, '', $input);
-		} else if (self::isShortUrl($input)) {
-			$words = str_replace(self::LINK_SHORT, '', $input);
-		} else if (self::isWords($input)) {
-			$words = $input;
+		if ($words = Helper::validateWords($this->input)) {
+			$this->data->words = $words;
+			return true;
 		}
-		if ($words) {
-			$words = urldecode($words);
-			$w3wApi = Factory::WhatThreeWords();
-			$response = $w3wApi->convertToCoordinates($words);
-			$error = $w3wApi->getError();
-			if ($error) {
-				// @TODO dirty hack to get stdclass instead of associated array
-				$error = json_decode(json_encode($error));
-				if ($error->code === 'BadWords') {
-					throw new InvalidLocationException(sprintf('What3Words "%s" are not valid words: "%s"', $words, $error->message));
-				} else if ($error->code === 'InvalidKey') {
-					throw new InvalidApiKeyException(sprintf('What3Words API responded with error: "%s"', $error->message));
-				} else {
-					throw new InvalidLocationException(sprintf('Detected What3Words "%s" but unable to get coordinates, invalid response from API: "%s"', $words, $error->message));
-				}
-			} else {
-				// @TODO dirty hack to get stdclass instead of associated array
-				$data = json_decode(json_encode($response));
-				$betterLocation = new BetterLocation($input, $data->coordinates->lat, $data->coordinates->lng, self::class);
-				$betterLocation->setPrefixMessage(sprintf('<a href="%s">%s</a>: <code>%s</code>', $data->map, self::NAME, $data->words));
-				return $betterLocation;
+		return false;
+	}
+
+	/**
+	 * @example https://w3w.co/chladná.naopak.vložit
+	 * @example https://what3words.com/define.readings.cucumber
+	 */
+	private function isUrl(): bool
+	{
+		if (
+			$this->url &&
+			Arrays::contains(['what3words.com', 'w3w.co'], $this->url->getDomain(2))
+		) {
+			$words = ltrim(urldecode($this->url->getPath()), '/');
+			if ($words = Helper::validateWords($words)) {
+				$this->data->words = $words;
+				return true;
 			}
-		} else {
-			throw new InvalidLocationException(sprintf('Unable to get coords from What3Words "%s".', $input));
 		}
-	}
-
-	public static function isWords(string $words): bool
-	{
-		// ///chladná.naopak.vložit
-		// \\\flicks.gazed.tapes
-		return !!(preg_match_all(self::RE, $words));
-	}
-
-	public static function isShortUrl(string $url): bool
-	{
-		// https://w3w.co/chladná.naopak.vložit
-		return (substr($url, 0, mb_strlen(self::LINK_SHORT)) === self::LINK_SHORT);
-	}
-
-	public static function isNormalUrl(string $url): bool
-	{
-		// https://what3words.com/define.readings.cucumber
-		return (substr($url, 0, mb_strlen(self::LINK)) === self::LINK);
-	}
-
-	/**
-	 * @param string $input
-	 * @return BetterLocationCollection
-	 * @throws NotImplementedException
-	 */
-	public static function parseCoordsMultiple(string $input): BetterLocationCollection
-	{
-		throw new NotImplementedException('Parsing multiple coordinates is not available.');
+		return false;
 	}
 }
