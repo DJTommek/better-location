304d7e614ebab4b2f99bb7c6a40509775f4e8602
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-03-11 01:16:19 +0100

    #46 Services refactoring: fixed MGRS and EnvironmentSettings tests

--- tests/BetterLocation/Service/MGRSServiceTest.php
+++ tests/BetterLocation/Service/MGRSServiceTest.php
@@ -20,21 +20,20 @@ final class MGRSServiceTest extends TestCase
 		MGRSService::getLink(50.087451, 14.420671, true);
 	}
 
-	/** @noinspection PhpUnhandledExceptionInspection */
 	public function testValidLocation(): void
 	{
-		$this->assertSame('50.086359,14.408709', MGRSService::parseCoords('33UVR577484')->__toString()); // Prague
-		$this->assertSame('21.309433,-157.916867', MGRSService::parseCoords('4QFJ12345678')->__toString()); // https://en.wikipedia.org/wiki/Military_Grid_Reference_System
-		$this->assertSame('21.309433,-157.916867', MGRSService::parseCoords('04QFJ12345678')->__toString()); // https://en.wikipedia.org/wiki/Military_Grid_Reference_System
-		$this->assertSame('38.959391,-95.265482', MGRSService::parseCoords('15SUD0370514711')->__toString());
-		$this->assertSame('38.889801,-77.036543', MGRSService::parseCoords('18SUJ2337106519')->__toString());
-		$this->assertSame('60.775935,4.693467', MGRSService::parseCoords('31VEH92233902')->__toString()); // Edge of Norway
-		$this->assertSame('-34.051387,18.462069', MGRSService::parseCoords('34HBH65742924')->__toString()); // South Africa
-		$this->assertSame('-45.892917,170.503103', MGRSService::parseCoords('59GMK61451773')->__toString()); // New Zeland
+		$this->assertSame('50.086359,14.408709', MGRSService::processStatic('33UVR577484')->getFirst()->__toString()); // Prague
+		$this->assertSame('21.309433,-157.916867', MGRSService::processStatic('4QFJ12345678')->getFirst()->__toString()); // https://en.wikipedia.org/wiki/Military_Grid_Reference_System
+		$this->assertSame('21.309433,-157.916867', MGRSService::processStatic('04QFJ12345678')->getFirst()->__toString()); // https://en.wikipedia.org/wiki/Military_Grid_Reference_System
+		$this->assertSame('38.959391,-95.265482', MGRSService::processStatic('15SUD0370514711')->getFirst()->__toString());
+		$this->assertSame('38.889801,-77.036543', MGRSService::processStatic('18SUJ2337106519')->getFirst()->__toString());
+		$this->assertSame('60.775935,4.693467', MGRSService::processStatic('31VEH92233902')->getFirst()->__toString()); // Edge of Norway
+		$this->assertSame('-34.051387,18.462069', MGRSService::processStatic('34HBH65742924')->getFirst()->__toString()); // South Africa
+		$this->assertSame('-45.892917,170.503103', MGRSService::processStatic('59GMK61451773')->getFirst()->__toString()); // New Zeland
 		// examples from https://www.usna.edu/Users/oceano/pguth/md_help/html/mgrs_utm.htm
-//		$this->assertSame(',', MGRSService::parseCoords('18SUJ7082315291')->__toString());
-//		$this->assertSame(',', MGRSService::parseCoords('18SUJ70821529')->__toString());
-//		$this->assertSame(',', MGRSService::parseCoords('18SUJ708152')->__toString());
+//		$this->assertSame(',', MGRSService::processStatic('18SUJ7082315291')->getFirst()->__toString());
+//		$this->assertSame(',', MGRSService::processStatic('18SUJ70821529')->getFirst()->__toString());
+//		$this->assertSame(',', MGRSService::processStatic('18SUJ708152')->getFirst()->__toString());
 	}
 
 	public function testNothingInText(): void
