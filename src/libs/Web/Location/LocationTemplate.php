e136e333bc9dbd827de20dcd9ea7600ed8e7044b
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-06-22 21:35:03 +0200

    #66 Website location: changed design, added icons, added support for adding CSS and JS only for specific pages

--- src/libs/Web/Location/LocationTemplate.php
+++ src/libs/Web/Location/LocationTemplate.php
@@ -3,13 +3,38 @@
 namespace App\Web\Location;
 
 use App\BetterLocation\BetterLocation;
-use App\BetterLocation\Service\DrobnePamatkyCzService;
+use App\BetterLocation\Service\GoogleMapsService;
+use App\BetterLocation\Service\HereWeGoService;
+use App\BetterLocation\Service\OpenStreetMapService;
+use App\BetterLocation\Service\WazeService;
 
 class LocationTemplate
 {
+	/** @var float */
+	public $lat;
+	/** @var float */
+	public $lon;
+
 	/** @var BetterLocation */
 	public $betterLocation;
 
 	public $websites = [];
+
+	public $linkWaze;
+	public $linkGoogle;
+	public $linkHere;
+	public $linkOSM;
+
+	public function __construct(BetterLocation $location)
+	{
+		$this->betterLocation = $location;
+		$this->lat = $location->getLat();
+		$this->lon = $location->getLon();
+
+		$this->linkWaze = WazeService::getLink($this->lat, $this->lon);
+		$this->linkGoogle = GoogleMapsService::getLink($this->lat, $this->lon);
+		$this->linkHere = HereWeGoService::getLink($this->lat, $this->lon);
+		$this->linkOSM = OpenStreetMapService::getLink($this->lat, $this->lon);
+	}
 }
 
