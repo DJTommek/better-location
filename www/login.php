44e312f3473ac8110b172b13a74e350f54c8b731
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-07-30 22:00:40 +0200

    #53 Telegram login: prepared templates, minor refactoring

--- www/login.php
+++ www/login.php
@@ -2,14 +2,5 @@
 
 require_once __DIR__ . '/../src/bootstrap.php';
 
-if (\App\TelegramCustomWrapper\Login::hasRequiredGetParams($_GET)) {
-	$login = new \App\TelegramCustomWrapper\Login($_GET);
-	if ($login->isVerified()) {
-		printf('Login was verified');
-	} else {
-		printf('Could not verify URL, try again');
-	}
-	dump($login);
-} else {
-	printf('Missing required data in GET parameter.');
-}
+$presenter = new \App\Web\Login\LoginPresenter();
+$presenter->render();
