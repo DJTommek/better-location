2ef57cc9e76bb438bc13aa7713d7c53a620057db
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-08-29 22:36:21 +0200

    #75 Web - multiple locations: implemented interactive map

--- src/libs/Web/Locations/LocationsTemplate.php
+++ src/libs/Web/Locations/LocationsTemplate.php
@@ -2,6 +2,7 @@
 
 namespace App\Web\Locations;
 
+use App\BetterLocation\BetterLocation;
 use App\BetterLocation\BetterLocationCollection;
 use App\Web\LayoutTemplate;
 
@@ -10,10 +11,24 @@ class LocationsTemplate extends LayoutTemplate
 	/** @var BetterLocationCollection */
 	public $collection;
 	public $websites = [];
+	public $allCoords = [];
+	public $collectionJs = [];
 
 	public function prepare(BetterLocationCollection $collection, array $websites)
 	{
 		$this->collection = $collection;
+		$this->collectionJs = array_map(function(BetterLocation $location) {
+			return [
+				'lat' => $location->getLat(),
+				'lon' => $location->getLon(),
+				'coords' => [$location->getLat(), $location->getLon()],
+				'hash' => $location->getCoordinates()->hash(),
+				'key' => $location->__toString(),
+			];
+		}, $collection->getLocations());
+		$this->allCoords = array_map(function(BetterLocation $location) {
+			return [$location->getLat(), $location->getLon()];
+		}, $collection->getLocations());
 		$this->websites = $websites;
 	}
 }
