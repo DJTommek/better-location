e023c983120ee679cba3d914064bc37d075db905
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-09-15 21:27:11 +0200

    Utils: added class TempFile()

---
+++ src/libs/Utils/TempFile.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace App\Utils;
+
+use App\Config;
+use Nette\Http\Url;
+use Nette\Http\UrlImmutable;
+use Nette\Utils\FileSystem;
+
+/**
+ * Create file with specific filename and content. File is automatically deleted once it is not referenced.
+ */
+class TempFile
+{
+	/** @var string Temporary directory, where temporary directories and files are created */
+	const TEMP_DIR = Config::FOLDER_TEMP . '/temp-file';
+
+	/** @var string */
+	private $filePath;
+	/** @var string */
+	private $dirPath;
+
+	/**
+	 * @param string $fileName
+	 * @param string|UrlImmutable|Url $content Content, which should be saved in file. If URL is provided, content is downloaded from that url.
+	 */
+	public function __construct(string $fileName, $content)
+	{
+		$this->dirPath = self::TEMP_DIR . '/' . uniqid();
+		$this->filePath = $this->dirPath . '/' . $fileName;
+		if ($content instanceof \Nette\Http\UrlImmutable || $content instanceof \Nette\Http\Url) {
+			$content = file_get_contents($content->getAbsoluteUrl());
+		}
+		FileSystem::write($this->filePath, $content);
+	}
+
+	public function __destruct()
+	{
+		FileSystem::delete($this->dirPath);
+	}
+
+	public function getFilePath(): string
+	{
+		return $this->filePath;
+	}
+}
