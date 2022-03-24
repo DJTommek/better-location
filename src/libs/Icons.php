316fb9e8f705cfc892305cb529dbea4365a85544
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-03-25 00:05:29 +0100

    cs

--- src/libs/Icons.php
+++ src/libs/Icons.php
@@ -4,46 +4,46 @@ namespace App;
 
 class Icons
 {
-	const ERROR = "\u{26D4}"; // https://emojipedia.org/no-entry/
-	const WARNING = "\u{26A0}"; // https://emojipedia.org/warning/
-	const SUCCESS = "\u{2705}"; // https://emojipedia.org/check-mark-button/
-	const CHECKED = "\u{2705}"; // https://emojipedia.org/check-mark-button/
-	const INFO = "\u{2139}"; // https://emojipedia.org/information/
-	const BACK = "\u{1F519}"; // https://emojipedia.org/back-arrow/
-	const REFRESH = "\u{267B}"; // https://emojipedia.org/recycling-symbol/
+	public const ERROR = "\u{26D4}"; // https://emojipedia.org/no-entry/
+	public const WARNING = "\u{26A0}"; // https://emojipedia.org/warning/
+	public const SUCCESS = "\u{2705}"; // https://emojipedia.org/check-mark-button/
+	public const CHECKED = "\u{2705}"; // https://emojipedia.org/check-mark-button/
+	public const INFO = "\u{2139}"; // https://emojipedia.org/information/
+	public const BACK = "\u{1F519}"; // https://emojipedia.org/back-arrow/
+	public const REFRESH = "\u{267B}"; // https://emojipedia.org/recycling-symbol/
 
-	const CHANGE = "\u{270F}"; // https://emojipedia.org/pencil/
-	const DELETE = "\u{1F5D1}"; // https://emojipedia.org/wastebasket/
+	public const CHANGE = "\u{270F}"; // https://emojipedia.org/pencil/
+	public const DELETE = "\u{1F5D1}"; // https://emojipedia.org/wastebasket/
 
-	const LOGIN = "\u{1F511}"; // https://emojipedia.org/key/
-	const LOGOUT = "\u{1F512}"; // https://emojipedia.org/locked/
+	public const LOGIN = "\u{1F511}"; // https://emojipedia.org/key/
+	public const LOGOUT = "\u{1F512}"; // https://emojipedia.org/locked/
 
-//	const ENABLED = "\u{1F7E2}"; // https://emojipedia.org/large-green-circle/ Warning: New in 2019 so not supported in older devices (eg. Android 9 or lower)
-	const ENABLED = "\u{2705}"; // https://emojipedia.org/check-mark-button/
-	const DISABLED = "\u{274C}"; // https://emojipedia.org/cross-mark/
+//	public const ENABLED = "\u{1F7E2}"; // https://emojipedia.org/large-green-circle/ Warning: New in 2019 so not supported in older devices (eg. Android 9 or lower)
+	public const ENABLED = "\u{2705}"; // https://emojipedia.org/check-mark-button/
+	public const DISABLED = "\u{274C}"; // https://emojipedia.org/cross-mark/
 
-	const USER = "\u{1F44D}"; // https://emojipedia.org/thumbs-up/
-	const GROUP = "\u{1F590}"; // https://emojipedia.org/hand-with-fingers-splayed/
-	const CHANNEL = "\u{1F4E2}"; // https://emojipedia.org/loudspeaker/
-	const FEATURES = "\u{1F4A1}"; // https://emojipedia.org/light-bulb/
-	const COMMAND = "\u{1F4AC}"; // https://emojipedia.org/speech-balloon/
-	const VIDEO = "\u{1F3A6}"; // https://emojipedia.org/cinema/
-	const FEEDBACK = "\u{1F5E3}"; // https://emojipedia.org/speaking-head/
+	public const USER = "\u{1F44D}"; // https://emojipedia.org/thumbs-up/
+	public const GROUP = "\u{1F590}"; // https://emojipedia.org/hand-with-fingers-splayed/
+	public const CHANNEL = "\u{1F4E2}"; // https://emojipedia.org/loudspeaker/
+	public const FEATURES = "\u{1F4A1}"; // https://emojipedia.org/light-bulb/
+	public const COMMAND = "\u{1F4AC}"; // https://emojipedia.org/speech-balloon/
+	public const VIDEO = "\u{1F3A6}"; // https://emojipedia.org/cinema/
+	public const FEEDBACK = "\u{1F5E3}"; // https://emojipedia.org/speaking-head/
 
-	const CAR = "\u{1F697}"; // https://emojipedia.org/automobile/
-	const PICTURE = "\u{1F5BC}"; // https://emojipedia.org/framed-picture/
-	const MAP_SCREEN = "\u{1F5FA}"; // https://emojipedia.org/world-map/
+	public const CAR = "\u{1F697}"; // https://emojipedia.org/automobile/
+	public const PICTURE = "\u{1F5BC}"; // https://emojipedia.org/framed-picture/
+	public const MAP_SCREEN = "\u{1F5FA}"; // https://emojipedia.org/world-map/
 
-	const ARROW_RIGHT = "\u{27A1}"; // https://emojipedia.org/right-arrow/
-	const ARROW_UP = "\u{2B06}"; // https://emojipedia.org/up-arrow/
+	public const ARROW_RIGHT = "\u{27A1}"; // https://emojipedia.org/right-arrow/
+	public const ARROW_UP = "\u{2B06}"; // https://emojipedia.org/up-arrow/
 
-	const LOCATION = "\u{1F4CD}"; // https://emojipedia.org/round-pushpin/
-	const INLINE = "\u{1F50D}"; // https://emojipedia.org/magnifying-glass-tilted-left/
+	public const LOCATION = "\u{1F4CD}"; // https://emojipedia.org/round-pushpin/
+	public const INLINE = "\u{1F50D}"; // https://emojipedia.org/magnifying-glass-tilted-left/
 
-	const CURRENT_LOCATION = "\u{1F3AF}"; // https://emojipedia.org/direct-hit/
+	public const CURRENT_LOCATION = "\u{1F3AF}"; // https://emojipedia.org/direct-hit/
 
-	const FAVOURITE = "\u{2764}"; // https://emojipedia.org/red-heart/
-	const FAVOURITE_REMOVE = "\u{1F494}"; // https://emojipedia.org/broken-heart/
+	public const FAVOURITE = "\u{2764}"; // https://emojipedia.org/red-heart/
+	public const FAVOURITE_REMOVE = "\u{1F494}"; // https://emojipedia.org/broken-heart/
 
-	const SETTINGS = "\u{2699}"; // https://emojipedia.org/gear/
+	public const SETTINGS = "\u{2699}"; // https://emojipedia.org/gear/
 }
