950d656448c74bb6af8deb2934b6ce693e295417
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-06-24 16:34:28 +0200

    #104 Service Waymarking.com: added tests

---
+++ tests/BetterLocation/Service/WaymarkingServiceTest.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace Tests\BetterLocation\Service;
+
+use App\BetterLocation\Service\Exceptions\NotSupportedException;
+use App\BetterLocation\Service\WaymarkingService;
+use PHPUnit\Framework\TestCase;
+
+final class WaymarkingServiceTest extends TestCase
+{
+	private function assertLocation(float $expectedLat, float $expectedLon, string $input)
+	{
+		$locations = WaymarkingService::processStatic($input)->getCollection();
+		$this->assertCount(1, $locations);
+		$location = $locations->getFirst();
+		$this->assertEqualsWithDelta($expectedLat, $location->getLat(), 0.000001);
+		$this->assertEqualsWithDelta($expectedLon, $location->getLon(), 0.000001);
+	}
+
+	public function testGenerateShareLink(): void
+	{
+		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=50.087451&lon=14.420671', WaymarkingService::getLink(50.087451, 14.420671));
+		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=50.100000&lon=14.500000', WaymarkingService::getLink(50.1, 14.5));
+		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=-50.200000&lon=14.600000', WaymarkingService::getLink(-50.2, 14.6000001)); // round down
+		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=50.300000&lon=-14.700001', WaymarkingService::getLink(50.3, -14.7000009)); // round up
+		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=-50.400000&lon=-14.800008', WaymarkingService::getLink(-50.4, -14.800008));
+	}
+
+	public function testGenerateDriveLink(): void
+	{
+		$this->expectException(NotSupportedException::class);
+		WaymarkingService::getLink(50.087451, 14.420671, true);
+	}
+
+	public function testIsUrl(): void
+	{
+		$this->assertTrue(WaymarkingService::isValidStatic('https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
+		$this->assertTrue(WaymarkingService::isValidStatic('https://waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
+		$this->assertTrue(WaymarkingService::isValidStatic('http://WWW.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
+		$this->assertTrue(WaymarkingService::isValidStatic('http://waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
+		$this->assertTrue(WaymarkingService::isValidStatic('https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
+		$this->assertTrue(WaymarkingService::isValidStatic('https://www.waymarking.com/waymarks/WMDPMB'));
+
+		$this->assertFalse(WaymarkingService::isValidStatic('not valid url'));
+		$this->assertFalse(WaymarkingService::isValidStatic('https://www.waymarking.com'));
+		$this->assertFalse(WaymarkingService::isValidStatic('https://www.waymarking.com/waymarks/'));
+		$this->assertFalse(WaymarkingService::isValidStatic('https://www.waymarking.com/waymarks/AADPMB'));
+	}
+
+	/**
+	 * @group request
+	 */
+	public function testWaymark(): void
+	{
+		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic');
+		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/waymarks/WMDPMB');
+		$this->assertLocation(42.618700, -82.477967, 'https://www.waymarking.com/waymarks/wm10');
+		$this->assertLocation(47.685467, -122.249950, 'https://www.waymarking.com/waymarks/wm2');
+	}
+
+	/**
+	 * @group request
+	 */
+	public function testImage(): void
+	{
+		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816&gid=3');
+		$this->assertLocation(42.618700, -82.477967, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=e1afc7aa-1dea-4110-9ae9-4d2b2c6deb68');
+	}
+}
