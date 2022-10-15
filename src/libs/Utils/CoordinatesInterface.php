ca3b3a50ba9550beeb3fd9027f1649715f23e0e6
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-10-15 23:10:34 +0200

    Coordinates interface: added key() and PHPDocs

--- src/libs/Utils/CoordinatesInterface.php
+++ src/libs/Utils/CoordinatesInterface.php
@@ -4,6 +4,18 @@ namespace App\Utils;
 
 interface CoordinatesInterface
 {
+	/**
+	 * Returns latitude in range from -90 to 90
+	 */
 	public function getLat(): float;
+
+	/**
+	 * Returns longitude in range from -180 to 180
+	 */
 	public function getLon(): float;
+
+	/**
+	 * Returns latitude and longitude in format 'lat,lon'
+	 */
+	public function key(): string;
 }
