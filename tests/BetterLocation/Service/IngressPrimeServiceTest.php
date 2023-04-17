35db8616a92124ebf94f1dd96a20653e6e13fe53
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-04-17 23:59:17 +0200

    IngressPrimeServiceTest: added missing '@group request' for processing

--- tests/BetterLocation/Service/IngressPrimeServiceTest.php
+++ tests/BetterLocation/Service/IngressPrimeServiceTest.php
@@ -37,6 +37,9 @@ final class IngressPrimeServiceTest extends TestCase
 		$this->assertFalse(IngressPrimeService::isValidStatic('https://link.ingress.com/?apn=com.nianticproject.ingress&isi=576505181&ibi=com.google.ingress&ifl=https%3A%2F%2Fapps.apple.com%2Fapp%2Fingress%2Fid576505181&ofl=https%3A%2F%2Fintel.ingress.com%2Fintel%3Fpll%3D50.087451%2C14999.420671'));
 	}
 
+	/**
+	 * @group request
+	 */
 	public function testProcess(): void
 	{
 		$collection = IngressPrimeService::processStatic('https://link.ingress.com/?link=https%3A%2F%2Fintel.ingress.com%2Fportal%2F0bd94fac5de84105b6eef6e7e1639ad9.12&apn=com.nianticproject.ingress&isi=576505181&ibi=com.google.ingress&ifl=https%3A%2F%2Fapps.apple.com%2Fapp%2Fingress%2Fid576505181&ofl=https%3A%2F%2Fintel.ingress.com%2Fintel%3Fpll%3D50.087451%2C14.420671')->getCollection();
