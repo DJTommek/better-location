4b419872d5f7b61949973c2ac024c92114eeb62c
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-04-20 22:11:33 +0200

    MiniCurl: reformatted Response class

--- src/libs/MiniCurl/Response.php
+++ src/libs/MiniCurl/Response.php
@@ -4,94 +4,94 @@ namespace App\MiniCurl;
 
 class Response
 {
-    private $raw;
-    private $body;
-    private $headers = [];
-    private $info;
-    /** @var false|\DateTimeImmutable */
-    private $cacheHit;
+	private $raw;
+	private $body;
+	private $headers = [];
+	private $info;
+	/** @var false|\DateTimeImmutable */
+	private $cacheHit;
 
-    public function __construct(string $rawResponse, array $curlInfo, $cacheHit = false)
-    {
-    	if ($cacheHit !== false && ($cacheHit instanceof \DateTimeImmutable === false)) { // @TODO use constructor union in PHP 8
+	public function __construct(string $rawResponse, array $curlInfo, $cacheHit = false)
+	{
+		if ($cacheHit !== false && ($cacheHit instanceof \DateTimeImmutable === false)) { // @TODO use constructor union in PHP 8
 			throw new \InvalidArgumentException(sprintf('Argument $cacheHit can be only false or DateTimeImmutable but "%s" provided.', gettype($cacheHit) === 'object' ? get_class($cacheHit) : gettype($cacheHit)));
-	    }
-        $this->cacheHit = $cacheHit;
-        $this->raw = $rawResponse;
-        list($headerString, $body) = explode("\r\n\r\n", $rawResponse, 2);
-        $this->body = $body;
-        $this->processHeaderString($headerString);
-        $this->info = $curlInfo;
-    }
+		}
+		$this->cacheHit = $cacheHit;
+		$this->raw = $rawResponse;
+		list($headerString, $body) = explode("\r\n\r\n", $rawResponse, 2);
+		$this->body = $body;
+		$this->processHeaderString($headerString);
+		$this->info = $curlInfo;
+	}
 
-    public function processHeaderString(string $headerString): void
-    {
-        $headers = explode("\r\n", $headerString);
-        array_shift($headers);
-        foreach ($headers as $header) {
-            list($headerName, $headerValue) = explode(': ', $header, 2);
-            $this->headers[mb_strtolower($headerName)] = $headerValue;
-        }
-    }
+	public function processHeaderString(string $headerString): void
+	{
+		$headers = explode("\r\n", $headerString);
+		array_shift($headers);
+		foreach ($headers as $header) {
+			list($headerName, $headerValue) = explode(': ', $header, 2);
+			$this->headers[mb_strtolower($headerName)] = $headerValue;
+		}
+	}
 
-    public function getRaw(): string
-    {
-        return $this->raw;
-    }
+	public function getRaw(): string
+	{
+		return $this->raw;
+	}
 
 	/** @return \DateTimeImmutable|false Datetime when cache was created or false if no cache was available or caching was disabled */
-    public function cacheHit()
-    {
-        return $this->cacheHit;
-    }
+	public function cacheHit()
+	{
+		return $this->cacheHit;
+	}
 
-    public function getBody(): string
-    {
-        return $this->body;
-    }
+	public function getBody(): string
+	{
+		return $this->body;
+	}
 
-    /** @TODO cache decoded json */
-    public function getBodyAsJson(bool $assoc = false, int $depth = 512, int $jsonOptions = JSON_THROW_ON_ERROR)
-    {
-        return json_decode($this->body, $assoc, $depth, $jsonOptions);
-    }
+	/** @TODO cache decoded json */
+	public function getBodyAsJson(bool $assoc = false, int $depth = 512, int $jsonOptions = JSON_THROW_ON_ERROR)
+	{
+		return json_decode($this->body, $assoc, $depth, $jsonOptions);
+	}
 
-    /**
-     * Get full curl_info() or one specific value (if not exists, return null)
-     *
-     * @param ?string $key Get value of specific key or set null to get all curl info as array
-     * @return array<string,mixed>|mixed|null
-     */
-    public function getInfo(?string $key = null)
-    {
-        if (is_null($key)) {
-            return $this->info;
-        } else if (isset($this->info[$key])) {
-            return $this->info[$key];
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * Get full curl_info() or one specific value (if not exists, return null)
+	 *
+	 * @param ?string $key Get value of specific key or set null to get all curl info as array
+	 * @return array<string,mixed>|mixed|null
+	 */
+	public function getInfo(?string $key = null)
+	{
+		if (is_null($key)) {
+			return $this->info;
+		} else if (isset($this->info[$key])) {
+			return $this->info[$key];
+		} else {
+			return null;
+		}
+	}
 
-    public function getCode(): int
-    {
-        return $this->getInfo('http_code');
-    }
+	public function getCode(): int
+	{
+		return $this->getInfo('http_code');
+	}
 
-    /**
-     * Get all headers or one specific header (if not exists, return null)
-     *
-     * @param ?string $key Get value of specific key or set null to get all headers as array
-     * @return array<string,string>|string|null
-     */
-    public function getHeaders(?string $key = null)
-    {
-        if (is_null($key)) {
-            return $this->headers;
-        } else if (isset($this->headers[$key])) {
-            return $this->headers[$key];
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * Get all headers or one specific header (if not exists, return null)
+	 *
+	 * @param ?string $key Get value of specific key or set null to get all headers as array
+	 * @return array<string,string>|string|null
+	 */
+	public function getHeaders(?string $key = null)
+	{
+		if (is_null($key)) {
+			return $this->headers;
+		} else if (isset($this->headers[$key])) {
+			return $this->headers[$key];
+		} else {
+			return null;
+		}
+	}
 }
