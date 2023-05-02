fc36d62725a9abd68c72db7be4f06cd7f110fa29
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-05-02 19:08:11 +0200

    Tests: refactored Ge0CodeTest

--- tests/Utils/Ge0CodeTest.php
+++ tests/Utils/Ge0CodeTest.php
@@ -4,9 +4,13 @@ namespace Tests\Utils;
 
 use App\Utils\Ge0Code;
 use PHPUnit\Framework\TestCase;
+use Tests\IteratorTrait;
+use Tests\TestUtils;
 
 final class Ge0CodeTest extends TestCase
 {
+	use IteratorTrait;
+
 	public function testDecode(): void
 	{
 		$result = Ge0Code::decode('B4srhdHVVt');
@@ -47,7 +51,6 @@ final class Ge0CodeTest extends TestCase
 	{
 		$this->assertSame('B4srhdHVVt', Ge0Code::encode(64.523401, 12.123401, 4.25)->code);
 		$this->assertSame('44G4Yn7Psx', Ge0Code::encode(50.042366, 14.454461, 18)->code);
-
 	}
 
 	public function testEncodeBorders(): void
@@ -58,38 +61,24 @@ final class Ge0CodeTest extends TestCase
 		$this->assertSame('AAAAAAAAAA', Ge0Code::encode(-90, -180, 4)->code);
 	}
 
-
 	/**
 	 * Generate random coordinate, convert them to code, this code convert back to coordinates and compare them with these randomly generated.
 	 * Aaaaaand do it multiple time.
+	 * @dataProvider iterator100Provider
 	 */
 	public function testRandom(): void
 	{
-		for ($i = 0; $i < 1000; $i++) {
-			$lat = $this->generateRandomLat();
-			$lon = $this->generateRandomLon();
-			$zoom = rand(4, 19);
-			$result = Ge0Code::encode($lat, $lon, $zoom);
-			$result2 = Ge0Code::decode($result->code);
+		$lat = TestUtils::randomLat();
+		$lon = TestUtils::randomLon();
+		$zoom = rand(4, 19);
+		$result = Ge0Code::encode($lat, $lon, $zoom);
+		$result2 = Ge0Code::decode($result->code);
 
-			$this->assertTrue(Ge0Code::isValid($result->code));
-			$this->assertSame($result->code, $result2->code);
-			$this->assertSame($result->zoom, $result2->zoom);
-			// precision might be lost while converting back and forth (eg 50.042366 can became 50.042365 or 50.042367)
-			$this->assertEqualsWithDelta($lat, $result2->lat, 0.00001);
-			$this->assertEqualsWithDelta($lon, $result2->lon, 0.00001);
-		}
+		$this->assertTrue(Ge0Code::isValid($result->code));
+		$this->assertSame($result->code, $result2->code);
+		$this->assertSame($result->zoom, $result2->zoom);
+		// precision might be lost while converting back and forth (eg 50.042366 can became 50.042365 or 50.042367)
+		$this->assertEqualsWithDelta($lat, $result2->lat, 0.00001);
+		$this->assertEqualsWithDelta($lon, $result2->lon, 0.00001);
 	}
-
-	private function generateRandomLat(): float
-	{
-		return rand(-89999999, 89999999) / 1000000;
-	}
-
-	private function generateRandomLon(): float
-	{
-		return rand(-179999999, 179999999) / 1000000;
-	}
-
-
 }
