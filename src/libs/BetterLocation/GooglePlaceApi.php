0472702cc752bcb90e2bf8b65cb9de2bf94eb280
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-06-27 19:33:37 +0200

    GooglePlaceApi: improved and refactored populating various data from placeDetails into BetterLocation

--- src/libs/BetterLocation/GooglePlaceApi.php
+++ src/libs/BetterLocation/GooglePlaceApi.php
@@ -189,38 +189,13 @@ class GooglePlaceApi
 
 			if ($loadPlaceDetails) {
 				try {
-					$placeDetails = $this->getPlaceDetails($placeCandidate->place_id, ['url', 'website', 'international_phone_number', 'business_status', 'address_components']);
+					$placeDetails = $this->getPlaceDetails($placeCandidate->place_id, ['name', 'formatted_address', 'url', 'website', 'international_phone_number', 'business_status', 'address_components']);
 					if ($placeDetails === null) {
 						if ($placeCandidate->name !== '') {
 							$betterLocation->setPrefixMessage($placeCandidate->name);
 						}
 					} else {
-						$betterLocation->setPrefixMessage(
-							sprintf(
-								'<a href="%s">%s</a>',
-								($placeDetails->website ?? $placeDetails->url),
-								$placeCandidate->name,
-							),
-						);
-
-						if (isset($placeDetails->business_status)) {
-							if ($placeDetails->business_status === self::BUSINESS_STATUS_CLOSED_TEMPORARILY) {
-								$betterLocation->addDescription(Icons::WARNING . ' Temporarily closed');
-							} else if ($placeDetails->business_status === self::BUSINESS_STATUS_CLOSED_PERMANENTLY) {
-								$betterLocation->addDescription(Icons::WARNING . ' Permanently closed');
-							}
-						}
-
-						if ($address !== null) {
-							if (isset($placeDetails->international_phone_number)) {
-								$address .= sprintf(' (%s)', $placeDetails->international_phone_number);
-							}
-
-							$countryCode = $this->getCountryCodeFromAddressComponents($placeDetails->address_components);
-							if ($countryCode !== null) {
-								$address = Utils::flagEmojiFromCountryCode($countryCode) . ' ' . $address;
-							}
-						}
+						self::populateLocationFromPlaceDetails($betterLocation, $placeDetails);
 					}
 				} catch (\Throwable $exception) {
 					Debugger::log($exception, ILogger::EXCEPTION);
@@ -234,6 +209,42 @@ class GooglePlaceApi
 		}
 		return $collection;
 	}
+
+	/**
+	 * Load data from placeDetails and insert it into provided BetterLocation
+	 */
+	public static function populateLocationFromPlaceDetails(BetterLocation $location, \stdClass $placeDetails): void {
+		$location->setPrefixMessage(
+			sprintf(
+				'<a href="%s">%s</a>',
+				($placeDetails->website ?? $placeDetails->url),
+				$placeDetails->name,
+			),
+		);
+
+		if (isset($placeDetails->business_status)) {
+			if ($placeDetails->business_status === self::BUSINESS_STATUS_CLOSED_TEMPORARILY) {
+				$location->addDescription(Icons::WARNING . ' Temporarily closed');
+			} else if ($placeDetails->business_status === self::BUSINESS_STATUS_CLOSED_PERMANENTLY) {
+				$location->addDescription(Icons::WARNING . ' Permanently closed');
+			}
+		}
+
+		assert(isset($placeDetails->formatted_address));
+		$address = $placeDetails->formatted_address;
+
+		if (isset($placeDetails->international_phone_number)) {
+			$address .= sprintf(' (%s)', $placeDetails->international_phone_number);
+		}
+
+		$countryCode = self::getCountryCodeFromAddressComponents($placeDetails->address_components);
+		if ($countryCode !== null) {
+			$address = Utils::flagEmojiFromCountryCode($countryCode) . ' ' . $address;
+		}
+		$location->setAddress($address);
+
+	}
+
 
 	/**
 	 * Helper method to do all logic and return collection of locations
@@ -257,7 +268,7 @@ class GooglePlaceApi
 	/**
 	 * @param array<object{long_name: string, short_name: string, types: array<string>}> $addressComponents
 	 */
-	private function getCountryCodeFromAddressComponents(array $addressComponents): ?string
+	public static function getCountryCodeFromAddressComponents(array $addressComponents): ?string
 	{
 		foreach ($addressComponents as $addressComponent) {
 			if (in_array(GeocodeResponse::ADDRESS_COMPONENT_COUNTRY, $addressComponent->types, true)) {
