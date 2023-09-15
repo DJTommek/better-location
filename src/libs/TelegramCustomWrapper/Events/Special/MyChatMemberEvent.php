24a268bec8a8d3b6ad6907d8e3638bd43908af2d
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-09-15 21:44:36 +0200

    Telegram: added handling adding bot to channel

--- src/libs/TelegramCustomWrapper/Events/Special/MyChatMemberEvent.php
+++ src/libs/TelegramCustomWrapper/Events/Special/MyChatMemberEvent.php
@@ -2,6 +2,9 @@
 
 namespace App\TelegramCustomWrapper\Events\Special;
 
+use App\Config;
+use App\TelegramCustomWrapper\SendMessage;
+use App\TelegramCustomWrapper\TelegramHelper as TH;
 use unreal4u\TelegramAPI\Telegram;
 
 /**
@@ -26,7 +29,50 @@ class MyChatMemberEvent extends Special
 
 	public function handleWebhookUpdate(): void
 	{
-		// Do nothing
+		$chat = $this->getTgChat();
+		$old = $this->update->my_chat_member->old_chat_member;
+		$new = $this->update->my_chat_member->new_chat_member;
+		if (
+			$chat->type === Telegram\Types\Chat::TYPE_CHANNEL
+			&& $old instanceof Telegram\Types\ChatMember\ChatMemberAdministrator === false
+			&& $new instanceof Telegram\Types\ChatMember\ChatMemberAdministrator === true
+		) {
+			$this->handleAddedToChannel();
+		}
+	}
+
+	private function handleAddedToChannel(): void
+	{
+		$admin = $this->getTgFrom();
+		$chat = $this->getTgChat();
+
+		$adminDisplayname = TH::getUserDisplayname($admin);
+		$channelDisplayname = TH::getChatDisplayname($chat);
+		$chatSettingsUrl = Config::getAppUrl('/chat/' . $this->getTgChatId());
+
+		$text = sprintf(
+				'Hi <b>%s</b>, thanks for adding me to channel <b>%s</b>.',
+				$adminDisplayname,
+				$channelDisplayname,
+			) . TH::NL;
+		$text .= 'I will be checking every post if it contains any form of location (coordinates, links, photos with EXIF...) and send a nicely formatted message.' . TH::NL;
+		$text .= TH::NL;
+		$text .= sprintf(
+				'To update settings use <a href="%s" target="_blank">website</a> (commands in channels are ignored).',
+				$chatSettingsUrl,
+			);
+
+		$replyMarkup = new Telegram\Types\Inline\Keyboard\Markup();
+		$replyMarkup->inline_keyboard[] = [
+			TH::loginUrlButton('Open settings', $chatSettingsUrl),
+		];
+
+		$sendMessage = new SendMessage(
+			chatId: $admin->id,
+			text: $text,
+			replyMarkup: $replyMarkup,
+		);
+		$this->run($sendMessage->msg);
 	}
 }
 
