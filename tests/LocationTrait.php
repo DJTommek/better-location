cf44f4f6c25750eeed1b827a7e35df6b92be7038
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-01-23 21:47:52 +0100

    Tests: added assertSingleLocation() method into LocationTrait

--- tests/LocationTrait.php
+++ tests/LocationTrait.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\BetterLocation\BetterLocationCollection;
 use DJTommek\Coordinates\CoordinatesInterface;
 
 trait LocationTrait
@@ -9,6 +10,8 @@ trait LocationTrait
 	public abstract static function assertSame($expected, $actual, string $message = ''): void;
 
 	public abstract static function assertEqualsWithDelta($expected, $actual, float $delta, string $message = ''): void;
+
+	public abstract static function assertCount(int $expectedCount, $haystack, string $message = ''): void;
 
 	protected function assertCoords(float $expectedLat, float $expectedLon, CoordinatesInterface $location): void
 	{
@@ -20,5 +23,13 @@ trait LocationTrait
 	{
 		$this->assertEqualsWithDelta($expectedLat, $location->getLat(), $delta);
 		$this->assertEqualsWithDelta($expectedLon, $location->getLon(), $delta);
+	}
+
+	protected function assertOneInCollection(float $expectedLat, float $expectedLon, ?string $sourceType, BetterLocationCollection $collection): void
+	{
+		$this->assertCount(1, $collection);
+		$location = $collection->getFirst();
+		$this->assertCoordsWithDelta($expectedLat, $expectedLon, $location);
+		$this->assertSame($sourceType, $location->getSourceType());
 	}
 }
