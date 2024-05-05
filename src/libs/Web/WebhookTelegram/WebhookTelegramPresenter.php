226c482386358eab2c299fe76ebe218f840ca5bb
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-05-05 15:19:23 +0200

    Container: Telegram Webhook presenter refactored to use container

--- src/libs/Web/WebhookTelegram/WebhookTelegramPresenter.php
+++ src/libs/Web/WebhookTelegram/WebhookTelegramPresenter.php
@@ -3,15 +3,25 @@
 namespace App\Web\WebhookTelegram;
 
 use App\Config;
+use App\Logger\CustomTelegramLogger;
 use App\TelegramCustomWrapper\Exceptions\EventNotSupportedException;
+use App\TelegramCustomWrapper\TelegramCustomWrapper;
 use App\TelegramCustomWrapper\TelegramHelper;
 use App\Utils\SimpleLogger;
 use App\Web\MainPresenter;
 use Nette\Utils\Json;
+use Tracy\Debugger;
 use unreal4u\TelegramAPI\Telegram;
 
 class WebhookTelegramPresenter extends MainPresenter
 {
+	public function __construct(
+		private readonly TelegramCustomWrapper $telegramCustomWrapper,
+		private readonly CustomTelegramLogger $telegramCustomLogger,
+	) {
+
+	}
+
 	public function action(): void
 	{
 		\Tracy\Debugger::enable(\Tracy\Debugger::Production, Config::getTracyPath());
@@ -30,20 +40,22 @@ class WebhookTelegramPresenter extends MainPresenter
 		}
 
 		try {
-			$updateData = Json::decode($input, Json::FORCE_ARRAY);
-			SimpleLogger::log(SimpleLogger::NAME_TELEGRAM_INPUT, $updateData);
+			$updateRaw = Json::decode($input, true);
+		} catch (\JsonException $exception) {
+			Debugger::log($exception);
+			$this->renderReply(self::HTTP_BAD_REQUEST, sprintf('Error: Telegram webhook API data are not valid JSON, error: "%s".', $exception->getMessage()));
+		}
+
+		try {
+			SimpleLogger::log(SimpleLogger::NAME_TELEGRAM_INPUT, $updateRaw);
 
 			\App\Factory::database(); // Just check if database connection is valid, otherwise throw Exception and end script now.
 
-			$telegramCustomWrapper = \App\Factory::telegram();
-			$update = new \unreal4u\TelegramAPI\Telegram\Types\Update(
-				$updateData,
-				\App\Factory::telegramCustomLogger(),
-			);
-			$event = $telegramCustomWrapper->analyze($update);
+			$update = new \unreal4u\TelegramAPI\Telegram\Types\Update($updateRaw, $this->telegramCustomLogger);
+			$event = $this->telegramCustomWrapper->analyze($update);
 			$timerName = 'eventHandling';
 			\Tracy\Debugger::timer($timerName);
-			$telegramCustomWrapper->executeEventHandler($event);
+			$this->telegramCustomWrapper->executeEventHandler($event);
 			\Tracy\Debugger::log(sprintf(
 				'Handling event %s took %F seconds. Log ID = %d',
 				$event::class,
@@ -51,16 +63,16 @@ class WebhookTelegramPresenter extends MainPresenter
 				LOG_ID,
 			),
 				\Tracy\Debugger::DEBUG);
-			printf('OK.');
+
+			$this->renderReply(self::HTTP_OK, 'Ok, end.');
 		} catch (EventNotSupportedException $exception) {
-			printf('<p>%s</p>', $exception->getMessage());
+			$this->renderReply(self::HTTP_OK, sprintf('%s', $exception->getMessage()));
 		} catch (\Throwable $exception) {
 			if ($this->request->getQuery('exception') === '0') {
-				printf('Error: "%s".', $exception->getMessage());
-			} else {
-				/** @noinspection PhpUnhandledExceptionInspection */
-				throw $exception;
+				$this->renderReply(self::HTTP_INTERNAL_SERVER_ERROR, sprintf('Error: "%s".', $exception->getMessage()));
 			}
+
+			throw $exception;
 		}
 	}
 
