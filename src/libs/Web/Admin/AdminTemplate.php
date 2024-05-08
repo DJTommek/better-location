9d3815178ddd4499bc486618e46e5acac1505315
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-05-08 10:21:25 +0200

    Admin Web: Latte template split into multiple templates

--- src/libs/Web/Admin/AdminTemplate.php
+++ src/libs/Web/Admin/AdminTemplate.php
@@ -4,6 +4,7 @@ namespace App\Web\Admin;
 
 use App\Config;
 use App\DefaultConfig;
+use App\TelegramUpdateDb;
 use App\Web\LayoutTemplate;
 use Nette\Http\Request;
 use Nette\Http\UrlImmutable;
@@ -15,11 +16,21 @@ class AdminTemplate extends LayoutTemplate
 	public readonly UrlImmutable $appUrl;
 	public readonly UrlImmutable $tgWebhookUrl;
 
+	public readonly int $autorefreshAllCount;
+	public readonly ?TelegramUpdateDb $newestRefresh;
+	public readonly ?TelegramUpdateDb $oldestRefresh;
+
+
 	public function prepare(Request $request): void
 	{
 		$this->request = $request;
 		$this->appUrl = Config::getAppUrl();
 		$this->isAppUrlSet = $this->appUrl->isEqual(DefaultConfig::getAppUrl()) === false;
 		$this->tgWebhookUrl = Config::getTelegramWebhookUrl();
+
+		$autorefreshAll = \App\TelegramUpdateDb::loadAll(\App\TelegramUpdateDb::STATUS_ENABLED);
+		$this->autorefreshAllCount = count($autorefreshAll);
+		$this->oldestRefresh = $autorefreshAll[0] ?? null;
+		$this->newestRefresh = $autorefreshAll[$this->autorefreshAllCount - 1] ?? null;
 	}
 }
