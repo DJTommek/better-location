5957e2abfd85e5fe54b7b452c4fdfc389aec78fd
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-05-14 17:34:06 +0200

    CronRefresh: refactored into presenter

---
+++ src/libs/Web/Maintenance/CronRefreshPresenter.php
@@ -0,0 +1,132 @@
+<?php declare(strict_types=1);
+
+namespace App\Web\Maintenance;
+
+use App\BetterLocation\BetterLocation;
+use App\Config;
+use App\Icons;
+use App\TelegramCustomWrapper\ProcessedMessageResult;
+use App\TelegramCustomWrapper\TelegramHelper;
+use App\Utils\Formatter;
+use App\Web\MainPresenter;
+use unreal4u\TelegramAPI\Telegram;
+
+use function Clue\React\Block\await;
+
+class CronRefreshPresenter extends MainPresenter
+{
+	/**
+	 * @var list<string>
+	 */
+	private array $log;
+
+	public function action(): void
+	{
+		$this->log = [];
+
+		if ($this->request->getQuery('password') !== \App\Config::CRON_PASSWORD) {
+			$this->apiResponse(true, 'Invalid password', httpCode: self::HTTP_FORBIDDEN);
+		}
+
+		$this->run2();
+
+		$result = new \stdClass();
+		$result->messages = $this->log;
+
+		$this->apiResponse(false, 'Done', $result);
+	}
+
+	private function run2(): void
+	{
+		$loop = \React\EventLoop\Factory::create();
+		$tgLog = new \unreal4u\TelegramAPI\TgLog(Config::TELEGRAM_BOT_TOKEN, new \unreal4u\TelegramAPI\HttpClientRequestHandler($loop));
+
+		$messagesToRefresh = \App\TelegramUpdateDb::loadAll(
+			\App\TelegramUpdateDb::STATUS_ENABLED,
+			null,
+			Config::REFRESH_CRON_MAX_UPDATES,
+			(new \DateTime())->sub(new \DateInterval(sprintf('PT%dS', Config::REFRESH_CRON_MIN_OLD))),
+		);
+
+		if (count($messagesToRefresh) === 0) {
+			$this->printlog('No message need refresh');
+			return;
+		}
+
+		$this->printlog(sprintf('Loaded %s updates to refresh.', count($messagesToRefresh)));
+		$telegramCustomWrapper = \App\Factory::telegram();
+		foreach ($messagesToRefresh as $messageToRefresh) {
+			$id = sprintf('%d-%d', $messageToRefresh->telegramChatId, $messageToRefresh->messageIdToEdit);
+			try {
+				$event = $telegramCustomWrapper->analyze($messageToRefresh->originalUpdateObject);
+				$this->printlog(sprintf('Processing %s with last refresh at %s (%s ago)',
+					$id,
+					$messageToRefresh->getLastUpdate()->format(DATE_W3C),
+					Formatter::ago($messageToRefresh->getLastUpdate()),
+				));
+				$collection = $event->getCollection();
+
+				if ($collection === null) {
+					$errorMessage = sprintf(
+						'Event "%s" does not have support for processing collections, skipping update %s.',
+						$event::class,
+						$id,
+					);
+					$this->printlog($errorMessage);
+					\Tracy\Debugger::log($errorMessage, \Tracy\Debugger::WARNING);
+					continue;
+				}
+
+				$processedCollection = new ProcessedMessageResult($collection, $event->getMessageSettings(), $event->getPluginer());
+				$processedCollection->setAutorefresh(true);
+				$processedCollection->process();
+
+				$msg = new \unreal4u\TelegramAPI\Telegram\Methods\EditMessageText();
+				$msg->chat_id = $messageToRefresh->telegramChatId;
+				$msg->message_id = $messageToRefresh->messageIdToEdit;
+				$msg->parse_mode = 'HTML';
+				$msg->disable_web_page_preview = true;
+
+				// remove last row where are located autorefresh buttons and replace this row with disabled state
+				$lastAutorefreshMarkup = $messageToRefresh->getLastResponseReplyMarkup();
+				if ($lastAutorefreshMarkup) {
+					array_pop($lastAutorefreshMarkup->inline_keyboard);
+					$lastAutorefreshMarkup->inline_keyboard[] = BetterLocation::generateRefreshButtons(false);
+				}
+
+				if (count($collection->getLocations()) === 0) {
+					$this->printlog(sprintf('Update %s don\'t have any locations anymore, disabling autorefresh.', $id));
+					$msg->text = $messageToRefresh->getLastResponseText() . sprintf('%s Last autorefresh at %s didn\'t detect any locations. Autorefreshing was disabled but you can try to enable it again.', Icons::REFRESH, (new \DateTimeImmutable())->format(Config::DATETIME_FORMAT_ZONE));
+					$msg->reply_markup = $lastAutorefreshMarkup;
+					$messageToRefresh->autorefreshDisable();
+					await($tgLog->performApiRequest($msg), $loop);
+				} else {
+					$replyMarkup = $processedCollection->getMarkup(1);
+					$text = $processedCollection->getText();
+					$msg->text = $text . sprintf('%s Autorefreshed: %s', Icons::REFRESH, (new \DateTimeImmutable())->format(Config::DATETIME_FORMAT_ZONE));
+					$msg->reply_markup = $replyMarkup;
+					await($tgLog->performApiRequest($msg), $loop);
+					$messageToRefresh->setLastSendData($text, $replyMarkup, true);
+					$this->printlog(sprintf('Update %s was processed.', $id));
+					$messageToRefresh->touchLastUpdate();
+				}
+			} catch (\Throwable $exception) {
+				if ($exception instanceof \unreal4u\TelegramAPI\Exceptions\ClientException && $exception->getMessage() === TelegramHelper::MESSAGE_TO_EDIT_DELETED) {
+					$messageToRefresh->autorefreshDisable();
+					$this->printlog(sprintf('Message %s, which should be edited was deleted, disabling autorefresh.', $id));
+				} else {
+					$this->printlog(sprintf('Exception occured while processing %s: %s', $id, $exception->getMessage()));
+					\Tracy\Debugger::log($exception, \Tracy\ILogger::EXCEPTION);
+				}
+			}
+		}
+		$this->printlog('All updates were processed');
+	}
+
+	private function printlog(string $text): void
+	{
+		$this->log[] = sprintf('%s: %s', (new \DateTime())->format(DATE_W3C), $text);
+		\App\Utils\SimpleLogger::log(\App\Utils\SimpleLogger::NAME_CRON_AUTOREFRESH, $text);
+	}
+}
+
