651fce1e6579a0292aae5f7180dc5c9dfd91e0db
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-06-01 00:23:02 +0200

    Rector: installed and setup `rector/rector`, added `composer rector` and `composer rector-dry` commands, enabled for www directory

---
+++ src/rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+	$rectorConfig->paths([
+//		__DIR__ . '/libs',
+//		__DIR__ . '/../tests',
+		__DIR__ . '/../www',
+	]);
+
+	// define sets of rules
+	$rectorConfig->sets([
+		LevelSetList::UP_TO_PHP_82,
+	]);
+
+	$rectorConfig->skip([
+		\Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
+	]);
+};
