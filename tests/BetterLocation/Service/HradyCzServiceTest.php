0dd364356aafb4816d2de398f48e5d0d95258711
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-06-25 19:01:41 +0200

    HradyCz service: refactored tests into data providers

--- tests/BetterLocation/Service/HradyCzServiceTest.php
+++ tests/BetterLocation/Service/HradyCzServiceTest.php
@@ -2,69 +2,93 @@
 
 namespace Tests\BetterLocation\Service;
 
-use App\BetterLocation\Service\Exceptions\NotSupportedException;
 use App\BetterLocation\Service\HradyCzService;
 use App\MiniCurl\Exceptions\InvalidResponseException;
-use PHPUnit\Framework\TestCase;
 
-final class HradyCzServiceTest extends TestCase
+final class HradyCzServiceTest extends AbstractServiceTestCase
 {
-	public function testGenerateShareLink(): void
+	protected function getServiceClass(): string
 	{
-		$this->expectException(NotSupportedException::class);
-		HradyCzService::getLink(50.087451, 14.420671);
+		return HradyCzService::class;
 	}
 
-	public function testGenerateDriveLink(): void
+	protected function getShareLinks(): array
 	{
-		$this->expectException(NotSupportedException::class);
-		HradyCzService::getLink(50.087451, 14.420671, true);
+		return [];
 	}
 
-	public function testIsValid(): void
+	protected function getDriveLinks(): array
 	{
-		$this->assertTrue(HradyCzService::validateStatic('https://www.hrady.cz/aaa-bbb-ccc'));
-		$this->assertTrue(HradyCzService::validateStatic('https://www.hrady.cz/certovy-hlavy-zelizy'));
-		$this->assertTrue(HradyCzService::validateStatic('https://hrady.cz/certovy-hlavy-zelizy'));
-		$this->assertTrue(HradyCzService::validateStatic('http://hrady.cz/certovy-hlavy-zelizy'));
-		$this->assertTrue(HradyCzService::validateStatic('https://www.hrady.cz/certovy-hlavy-zelizy/'));
-		$this->assertTrue(HradyCzService::validateStatic('https://www.hrady.cz/certovy-hlavy-zelizy/komentare'));
-		$this->assertTrue(HradyCzService::validateStatic('https://www.hrady.cz/certovy-hlavy-zelizy/komentare/new'));
-		$this->assertTrue(HradyCzService::validateStatic('https://www.hrady.cz/pevnost-bunkr-lo-vz-37-a-124az1z-vaha'));
+		return [];
+	}
 
-		$this->assertFalse(HradyCzService::validateStatic('some invalid url'));
-		$this->assertFalse(HradyCzService::validateStatic('https://www.hrady.cz/aaa-bbb'));
-		$this->assertFalse(HradyCzService::validateStatic('https://www.hrady.cz/mapa'));
-		$this->assertFalse(HradyCzService::validateStatic('https://www.hrady.cz/clanky/pohadkovemu-jicinu-predchazela-jedna-z-nejvetsich-katastrof-17-stoleti'));
-		$this->assertFalse(HradyCzService::validateStatic('https://www.hrady.cz/search?typ_dop=105'));
+	public static function isValidProvider(): array
+	{
+		return [
+			[true, 'https://www.hrady.cz/aaa-bbb-ccc'],
+			[true, 'https://www.hrady.cz/certovy-hlavy-zelizy'],
+			[true, 'https://hrady.cz/certovy-hlavy-zelizy'],
+			[true, 'http://hrady.cz/certovy-hlavy-zelizy'],
+			[true, 'https://www.hrady.cz/certovy-hlavy-zelizy/'],
+			[true, 'https://www.hrady.cz/certovy-hlavy-zelizy/komentare'],
+			[true, 'https://www.hrady.cz/certovy-hlavy-zelizy/komentare/new'],
+			[true, 'https://www.hrady.cz/pevnost-bunkr-lo-vz-37-a-124az1z-vaha'],
+
+			[false, 'some invalid url'],
+			[false, 'https://www.hrady.cz/aaa-bbb'],
+			[false, 'https://www.hrady.cz/mapa'],
+			[false, 'https://www.hrady.cz/clanky/pohadkovemu-jicinu-predchazela-jedna-z-nejvetsich-katastrof-17-stoleti'],
+			[false, 'https://www.hrady.cz/search?typ_dop=105'],
+		];
+	}
+
+	public static function processProvider(): array
+	{
+		return [
+			[50.420540, 14.464405, 'https://www.hrady.cz/certovy-hlavy-zelizy'],
+			[50.306440, 14.288090, 'https://www.hrady.cz/pevnost-bunkr-lo-vz-37-a-124az1z-vaha'],
+			[50.305519, 14.235415, 'https://www.hrady.cz/kaple-nanebevzeti-panny-marie-miletice/ubytovani'],
+		];
+	}
+
+	public static function processProviderInvalid(): array
+	{
+		return [
+			['https://www.hrady.cz/aaa-bbb-ccc'],
+		];
+	}
+
+	/**
+	 * @dataProvider isValidProvider
+	 */
+	public function testIsValid(bool $expectedIsValid, string $input): void
+	{
+		$service = new HradyCzService();
+		$this->assertServiceIsValid($service, $input, $expectedIsValid);
 	}
 
 	/**
 	 * @group request
+	 * @dataProvider processProvider
 	 */
-	public function testProcess(): void
+	public function testProcess(float $expectedLat, float $expectedLon, string $input): void
 	{
-		$collection = HradyCzService::processStatic('https://www.hrady.cz/certovy-hlavy-zelizy')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('50.420540,14.464405', $collection[0]->__toString());
-
-		$collection = HradyCzService::processStatic('https://www.hrady.cz/pevnost-bunkr-lo-vz-37-a-124az1z-vaha')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('50.306440,14.288090', $collection[0]->__toString());
-
-		$collection = HradyCzService::processStatic('https://www.hrady.cz/kaple-nanebevzeti-panny-marie-miletice/ubytovani')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('50.305519,14.235415', $collection[0]->__toString());
+		$service = new HradyCzService();
+		$this->assertServiceLocation($service, $input, $expectedLat, $expectedLon);
 	}
 
 	/**
 	 * @group request
+	 * @dataProvider processProviderInvalid
 	 */
-	public function testInvalidId(): void
+	public function testInvalidId(string $input): void
 	{
+		$service = new HradyCzService();
+
 		$this->expectException(InvalidResponseException::class);
 		$this->expectExceptionCode(404);
 		$this->expectExceptionMessage('Invalid response code "404" but required "200" for URL "www.hrady.cz".');
-		HradyCzService::processStatic('https://www.hrady.cz/aaa-bbb-ccc');
+
+		$this->assertServiceNoLocation($service, $input);
 	}
 }
