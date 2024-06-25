68768d4ffb91d94ba7e646360f0fc41df91903b9
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-06-25 21:38:14 +0200

    PrazdneDomyCz service: refactored tests into data providers

--- tests/BetterLocation/Service/PrazdneDomyCzServiceTest.php
+++ tests/BetterLocation/Service/PrazdneDomyCzServiceTest.php
@@ -4,48 +4,86 @@ namespace Tests\BetterLocation\Service;
 
 use App\BetterLocation\Service\PrazdneDomyCzService;
 use App\MiniCurl\Exceptions\InvalidResponseException;
-use PHPUnit\Framework\TestCase;
 
-final class PrazdneDomyCzServiceTest extends TestCase
+final class PrazdneDomyCzServiceTest extends AbstractServiceTestCase
 {
-	public function testIsValid(): void
+	protected function getServiceClass(): string
 	{
-		// Place
-		$this->assertTrue(PrazdneDomyCzService::validateStatic('https://prazdnedomy.cz/domy/objekty/detail/2732-kasarna-u-sloupu'));
-		$this->assertTrue(PrazdneDomyCzService::validateStatic('https://prazdnedomy.cz/domy/objekty/detail/96-dum-u-tri-bilych-lilii'));
-		$this->assertTrue(PrazdneDomyCzService::validateStatic('https://prazdnedomy.cz/domy/objekty/detail/96'));
-		$this->assertTrue(PrazdneDomyCzService::validateStatic('https://www.prazdnedomy.cz/domy/objekty/detail/96'));
-		$this->assertTrue(PrazdneDomyCzService::validateStatic('http://www.prazdnedomy.cz/domy/objekty/detail/96'));
+		return PrazdneDomyCzService::class;
+	}
 
-		// Invalid
-		$this->assertFalse(PrazdneDomyCzService::validateStatic('some invalid url'));
-		$this->assertFalse(PrazdneDomyCzService::validateStatic('https://prazdnedomy.cz/domy/objekty/detail/kasarna-u-sloupu'));
-		$this->assertFalse(PrazdneDomyCzService::validateStatic('https://prazdnedomy.cz/clanky/'));
-		$this->assertFalse(PrazdneDomyCzService::validateStatic('https://prazdnedomy.cz/clanky/prazdne-domy-na-vedlejsi-koleji-aneb-prazdna-nadrazi-jako-prilezitost/'));
+	protected function getShareLinks(): array
+	{
+		return [];
+	}
+
+	protected function getDriveLinks(): array
+	{
+		return [];
+	}
+
+	public static function isValidProvider(): array
+	{
+		return [
+			[true, 'https://prazdnedomy.cz/domy/objekty/detail/2732-kasarna-u-sloupu'],
+			[true, 'https://prazdnedomy.cz/domy/objekty/detail/96-dum-u-tri-bilych-lilii'],
+			[true, 'https://prazdnedomy.cz/domy/objekty/detail/96'],
+			[true, 'https://www.prazdnedomy.cz/domy/objekty/detail/96'],
+			[true, 'http://www.prazdnedomy.cz/domy/objekty/detail/96'],
+
+			[false, 'some invalid url'],
+			[false, 'https://prazdnedomy.cz/domy/objekty/detail/kasarna-u-sloupu'],
+			[false, 'https://prazdnedomy.cz/clanky/'],
+			[false, 'https://prazdnedomy.cz/clanky/prazdne-domy-na-vedlejsi-koleji-aneb-prazdna-nadrazi-jako-prilezitost/'],
+		];
+	}
+
+	public static function processProvider(): array
+	{
+		return [
+			[49.060201, 13.736212, 'https://prazdnedomy.cz/domy/objekty/detail/2732-kasarna-u-sloupu'],
+			[50.087720, 14.398980, 'https://prazdnedomy.cz/domy/objekty/detail/96-dum-u-tri-bilych-lilii'],
+		];
+	}
+
+	public static function processInvalidIdProvider(): array
+	{
+		return [
+			['https://prazdnedomy.cz/domy/objekty/detail/999999999'],
+		];
+	}
+
+	/**
+	 * @dataProvider isValidProvider
+	 */
+	public function testIsValid(bool $expectedIsValid, string $input): void
+	{
+		$service = new PrazdneDomyCzService();
+		$this->assertServiceIsValid($service, $input, $expectedIsValid);
 	}
 
 	/**
 	 * @group request
+	 * @dataProvider processProvider
 	 */
-	public function testProcessPlace(): void
+	public function testProcess(float $expectedLat, float $expectedLon, string $input): void
 	{
-		$collection = PrazdneDomyCzService::processStatic('https://prazdnedomy.cz/domy/objekty/detail/2732-kasarna-u-sloupu')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('49.060201,13.736212', $collection[0]->getLatLon());
-
-		$collection = PrazdneDomyCzService::processStatic('https://prazdnedomy.cz/domy/objekty/detail/96-dum-u-tri-bilych-lilii')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('50.087720,14.398980', $collection[0]->getLatLon());
+		$service = new PrazdneDomyCzService();
+		$this->assertServiceLocation($service, $input, $expectedLat, $expectedLon);
 	}
 
 	/**
 	 * @group request
+	 * @dataProvider processInvalidIdProvider
 	 */
-	public function testInvalidId(): void
+	public function testInvalidId(string $input): void
 	{
+		$service = new PrazdneDomyCzService();
+
 		$this->expectException(InvalidResponseException::class);
 		$this->expectExceptionCode(500);
 		$this->expectExceptionMessage('Invalid response code "500" but required "200" for URL "prazdnedomy.cz"');
-		PrazdneDomyCzService::processStatic('https://prazdnedomy.cz/domy/objekty/detail/999999999');
+
+		$this->assertServiceNoLocation($service, $input);
 	}
 }
