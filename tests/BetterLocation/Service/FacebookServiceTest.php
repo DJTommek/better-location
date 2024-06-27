f986a1fd01b3a4497745aa85f1206ce16052a919
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-06-27 19:43:06 +0200

    Facebook service: refactored tests into data providers

--- tests/BetterLocation/Service/FacebookServiceTest.php
+++ tests/BetterLocation/Service/FacebookServiceTest.php
@@ -2,87 +2,102 @@
 
 namespace Tests\BetterLocation\Service;
 
-use App\BetterLocation\Service\Exceptions\NotSupportedException;
 use App\BetterLocation\Service\FacebookService;
-use PHPUnit\Framework\TestCase;
 
-final class FacebookServiceTest extends TestCase
+final class FacebookServiceTest extends AbstractServiceTestCase
 {
-	public function testGenerateShareLink(): void
+	protected function getServiceClass(): string
 	{
-		$this->expectException(NotSupportedException::class);
-		FacebookService::getLink(50.087451, 14.420671);
+		return FacebookService::class;
 	}
 
-	public function testGenerateDriveLink(): void
+	protected function getShareLinks(): array
 	{
-		$this->expectException(NotSupportedException::class);
-		FacebookService::getLink(50.087451, 14.420671, true);
+		return [];
 	}
 
-	public function testIsUrl(): void
+	protected function getDriveLinks(): array
 	{
-		$this->assertTrue(FacebookService::validateStatic('https://facebook.com/burgerzelva'));
-		$this->assertTrue(FacebookService::validateStatic('http://facebook.com/burgerzelva'));
-		$this->assertTrue(FacebookService::validateStatic('http://www.facebook.com/burgerzelva'));
-		$this->assertTrue(FacebookService::validateStatic('https://www.facebook.com/burgerzelva'));
-		$this->assertTrue(FacebookService::validateStatic('https://facebook.com/burgerzelva/'));
-		$this->assertTrue(FacebookService::validateStatic('https://facebook.com/burgerzelva/menu'));
-		$this->assertTrue(FacebookService::validateStatic('https://facebook.com/burgerzelva/menu/?ref=page_internal'));
-		$this->assertTrue(FacebookService::validateStatic('https://facebook.com/burgerzelva?ref=page_internal'));
-		$this->assertTrue(FacebookService::validateStatic('https://m.facebook.com/burgerzelva'));
-		$this->assertTrue(FacebookService::validateStatic('https://pt-br.facebook.com/burgerzelva'));
-		$this->assertTrue(FacebookService::validateStatic('https://m.facebook.com/gentlegiantcafex/'));
-		$this->assertTrue(FacebookService::validateStatic('https://pt-br.facebook.com/fantaziecafe/'));
-		$this->assertTrue(FacebookService::validateStatic('https://www.facebook.com/FlotaVacaDiezSCZ/'));
-		$this->assertTrue(FacebookService::validateStatic('https://www.facebook.com/Bodegas-Alfaro-730504807012751/'));
-		$this->assertTrue(FacebookService::validateStatic('https://www.facebook.com/Biggie-Express-251025431718109/about/?ref=page_internal'));
+		return [];
+	}
 
-		$this->assertFalse(FacebookService::validateStatic('https://facebook.com/'));
-		$this->assertFalse(FacebookService::validateStatic('https://facebook.com'));
-		$this->assertFalse(FacebookService::validateStatic('https://facebook.com?foo=bar'));
+	public static function isValidUrlProvider(): array
+	{
+		return [
+			[true, 'https://facebook.com/burgerzelva'],
+			[true, 'http://facebook.com/burgerzelva'],
+			[true, 'http://www.facebook.com/burgerzelva'],
+			[true, 'https://www.facebook.com/burgerzelva'],
+			[true, 'https://facebook.com/burgerzelva/'],
+			[true, 'https://facebook.com/burgerzelva/menu'],
+			[true, 'https://facebook.com/burgerzelva/menu/?ref=page_internal'],
+			[true, 'https://facebook.com/burgerzelva?ref=page_internal'],
+			[true, 'https://m.facebook.com/burgerzelva'],
+			[true, 'https://pt-br.facebook.com/burgerzelva'],
+			[true, 'https://m.facebook.com/gentlegiantcafex/'],
+			[true, 'https://pt-br.facebook.com/fantaziecafe/'],
+			[true, 'https://www.facebook.com/FlotaVacaDiezSCZ/'],
+			[true, 'https://www.facebook.com/Bodegas-Alfaro-730504807012751/'],
+			[true, 'https://www.facebook.com/Biggie-Express-251025431718109/about/?ref=page_internal'],
 
-		$this->assertFalse(FacebookService::validateStatic('some invalid url'));
+			[false, 'https://facebook.com/'],
+			[false, 'https://facebook.com'],
+			[false, 'https://facebook.com?foo=bar'],
+
+			[false, 'some invalid url'],
+		];
+	}
+
+	public static function processUrlProvider(): array
+	{
+		return [
+
+			[50.087244, 14.469230, 'https://pt-br.facebook.com/burgerzelva/menu/?ref=page_internal'],
+			[50.061790, 14.437030, 'https://pt-br.facebook.com/fantaziecafe/'],
+			// [40.411600,-3.700390, 'https://www.facebook.com/Bodegas-Alfaro-730504807012751/'],
+			[-43.538899, 172.652603, 'https://m.facebook.com/gentlegiantcafex/'],
+			// [-25.285736,-57.559743, 'https://www.facebook.com/Biggie-Express-251025431718109/about/?ref=page_internal'],
+			[-17.792721, -63.155202, 'https://www.facebook.com/FlotaVacaDiezSCZ/'],
+		];
+	}
+
+	public static function processUrlNoLocationProvider(): array
+	{
+		return [
+			['https://www.facebook.com/ThePokeHaus'],
+		];
+	}
+
+	/**
+	 * @dataProvider isValidUrlProvider
+	 */
+	public function testIsValid(bool $expectedIsValid, string $input): void
+	{
+		$service = new FacebookService();
+		$this->assertServiceIsValid($service, $input, $expectedIsValid);
 	}
 
 	/**
 	 * @group request
+	 *
+	 * @dataProvider processUrlProvider
 	 */
-	public function testUrl(): void
+	public function testProcess(float $expectedLat, float $expectedLon, string $input): void
 	{
-		$this->markTestSkipped('Disabled due to possibly too many requests to Facebook servers');
-
-		$collection = FacebookService::processStatic('https://pt-br.facebook.com/burgerzelva/menu/?ref=page_internal')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('50.087244,14.469230', $collection[0]->__toString());
-
-		$collection = FacebookService::processStatic('https://pt-br.facebook.com/fantaziecafe/')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('50.061790,14.437030', $collection[0]->__toString());
-
-		$collection = FacebookService::processStatic('https://www.facebook.com/Bodegas-Alfaro-730504807012751/')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('40.411600,-3.700390', $collection[0]->__toString());
-
-		$collection = FacebookService::processStatic('https://m.facebook.com/gentlegiantcafex/')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('-43.538899,172.652603', $collection[0]->__toString());
-
-		$collection = FacebookService::processStatic('https://www.facebook.com/Biggie-Express-251025431718109/about/?ref=page_internal')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('-25.285736,-57.559743', $collection[0]->__toString());
-
-		$collection = FacebookService::processStatic('https://www.facebook.com/FlotaVacaDiezSCZ/')->getCollection();
-		$this->assertCount(1, $collection);
-		$this->assertSame('-17.792721,-63.155202', $collection[0]->__toString());
+		//		$this->markTestSkipped('Disabled due to possibly too many requests to Facebook servers');
+		$service = new FacebookService();
+		$this->assertServiceLocation($service, $input, $expectedLat, $expectedLon);
 	}
 
 	/**
 	 * @group request
+	 *
+	 * @dataProvider processUrlNoLocationProvider
 	 */
-	public function testMissingCoordinates(): void
+	public function testProcessNoLocation(string $input): void
 	{
-		$collection = FacebookService::processStatic('https://www.facebook.com/ThePokeHaus')->getCollection();
-		$this->assertCount(0, $collection);
+		//		$this->markTestSkipped('Disabled due to possibly too many requests to Facebook servers');
+		$service = new FacebookService();
+		$this->assertServiceNoLocation($service, $input);
 	}
 }
