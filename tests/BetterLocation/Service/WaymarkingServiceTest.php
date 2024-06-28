21c31cfa39dbc33aa6b1406ff90e06c37af537fb
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-06-28 17:54:19 +0200

    Waymarking service: refactored tests into data providers

--- tests/BetterLocation/Service/WaymarkingServiceTest.php
+++ tests/BetterLocation/Service/WaymarkingServiceTest.php
@@ -2,97 +2,121 @@
 
 namespace Tests\BetterLocation\Service;
 
-use App\BetterLocation\Service\Exceptions\NotSupportedException;
 use App\BetterLocation\Service\WaymarkingService;
-use PHPUnit\Framework\TestCase;
 
-final class WaymarkingServiceTest extends TestCase
+final class WaymarkingServiceTest extends AbstractServiceTestCase
 {
-	private function assertLocation(float $expectedLat, float $expectedLon, string $input): void
+	protected function getServiceClass(): string
 	{
-		$locations = WaymarkingService::processStatic($input)->getCollection();
-		$this->assertCount(1, $locations);
-		$location = $locations->getFirst();
-		$this->assertEqualsWithDelta($expectedLat, $location->getLat(), 0.000001);
-		$this->assertEqualsWithDelta($expectedLon, $location->getLon(), 0.000001);
+		return WaymarkingService::class;
 	}
 
-	public function testGenerateShareLink(): void
+	protected function getShareLinks(): array
 	{
-		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=50.087451&lon=14.420671', WaymarkingService::getLink(50.087451, 14.420671));
-		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=50.100000&lon=14.500000', WaymarkingService::getLink(50.1, 14.5));
-		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=-50.200000&lon=14.600000', WaymarkingService::getLink(-50.2, 14.6000001)); // round down
-		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=50.300000&lon=-14.700001', WaymarkingService::getLink(50.3, -14.7000009)); // round up
-		$this->assertSame('https://www.waymarking.com/wm/search.aspx?lat=-50.400000&lon=-14.800008', WaymarkingService::getLink(-50.4, -14.800008));
+		$this->revalidateGeneratedShareLink = false;
+
+		return [
+			'https://www.waymarking.com/wm/search.aspx?lat=50.087451&lon=14.420671',
+			'https://www.waymarking.com/wm/search.aspx?lat=50.100000&lon=14.500000',
+			'https://www.waymarking.com/wm/search.aspx?lat=-50.200000&lon=14.600000', // round down
+			'https://www.waymarking.com/wm/search.aspx?lat=50.300000&lon=-14.700001', // round up
+			'https://www.waymarking.com/wm/search.aspx?lat=-50.400000&lon=-14.800008',
+		];
 	}
 
-	public function testGenerateDriveLink(): void
+	protected function getDriveLinks(): array
 	{
-		$this->expectException(NotSupportedException::class);
-		WaymarkingService::getLink(50.087451, 14.420671, true);
+		return [];
 	}
 
-	public function testIsWaymarkUrl(): void
+	public static function isWaymarkUrlProvider(): array
 	{
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
-		$this->assertTrue(WaymarkingService::validateStatic('https://waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
-		$this->assertTrue(WaymarkingService::validateStatic('http://WWW.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
-		$this->assertTrue(WaymarkingService::validateStatic('http://waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'));
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/waymarks/WMDPMB'));
+		return [
+			[true, 'https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'],
+			[true, 'https://waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'],
+			[true, 'http://WWW.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'],
+			[true, 'http://waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'],
+			[true, 'https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'],
+			[true, 'https://www.waymarking.com/waymarks/WMDPMB'],
 
-		$this->assertFalse(WaymarkingService::validateStatic('not valid url'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com/waymarks/'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com/waymarks/AADPMB'));
+			[false, 'not valid url'],
+			[false, 'https://www.waymarking.com'],
+			[false, 'https://www.waymarking.com/waymarks/'],
+			[false, 'https://www.waymarking.com/waymarks/AADPMB'],
+		];
 	}
 
-	public function testIsImageUrl(): void
+	public static function isImageUrlProvider(): array
 	{
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816&gid=3'));
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816'));
+		return [
+			[true, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816&gid=3'],
+			[true, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816'],
 
-		$this->assertFalse(WaymarkingService::validateStatic('not valid url'));
+			[false, 'not valid url'],
+		];
 	}
 
-	public function testIsGalleryUrl(): void
+	public static function isGalleryUrlProvider(): array
 	{
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=2'));
-		$this->assertTrue(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/default.aspx?f=1&guid=01645b8d-404c-41f1-a951-190032f55215&gid=2'));
+		return [
+			[true, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=2'],
+			[true, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=01645b8d-404c-41f1-a951-190032f55215&gid=2'],
 
-		$this->assertFalse(WaymarkingService::validateStatic('not valid url'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9baaaaa&gid=2'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/default.aspx?f=2&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=2'));
-		$this->assertFalse(WaymarkingService::validateStatic('https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=3'));
+			[false, 'not valid url'],
+			[false, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b'],
+			[false, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9baaaaa&gid=2'],
+			[false, 'https://www.waymarking.com/gallery/default.aspx?f=2&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=2'],
+			[false, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=3'],
+		];
+	}
+
+	public static function processWaymarkUrlProvider(): array
+	{
+		return [
+			[49.198050, 16.607533, 'https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic'],
+			[49.198050, 16.607533, 'https://www.waymarking.com/waymarks/WMDPMB'],
+			[42.618700, -82.477967, 'https://www.waymarking.com/waymarks/wm10'],
+			[47.685467, -122.249950, 'https://www.waymarking.com/waymarks/wm2'],
+		];
+	}
+
+	public static function processImageUrlProvider(): array
+	{
+		return [
+			[49.198050, 16.607533, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816&gid=3'],
+			[42.618700, -82.477967, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=e1afc7aa-1dea-4110-9ae9-4d2b2c6deb68'],
+		];
+	}
+
+	public function processGalleryUrlProvider(): array
+	{
+		return [
+			[49.198050, 16.607533, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=2'],
+			[42.618700, -82.477967, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=01645b8d-404c-41f1-a951-190032f55215&gid=2'],
+		];
+	}
+
+	/**
+	 * @dataProvider isWaymarkUrlProvider
+	 * @dataProvider isImageUrlProvider
+	 * @dataProvider isGalleryUrlProvider
+	 */
+	public function testIsValid(bool $expectedIsValid, string $input): void
+	{
+		$service = new WaymarkingService();
+		$this->assertServiceIsValid($service, $input, $expectedIsValid);
 	}
 
 	/**
 	 * @group request
+	 *
+	 * @dataProvider processWaymarkUrlProvider
+	 * @dataProvider processImageUrlProvider
+	 * @dataProvider processGalleryUrlProvider
 	 */
-	public function testWaymark(): void
+	public function testProcessReal(float $expectedLat, float $expectedLon, string $input): void
 	{
-		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/waymarks/WMDPMB_Erb_Leopolda_Laanskho_Moravsk_nm_Brno_Czech_Republic');
-		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/waymarks/WMDPMB');
-		$this->assertLocation(42.618700, -82.477967, 'https://www.waymarking.com/waymarks/wm10');
-		$this->assertLocation(47.685467, -122.249950, 'https://www.waymarking.com/waymarks/wm2');
-	}
-
-	/**
-	 * @group request
-	 */
-	public function testImage(): void
-	{
-		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=14f4cb4b-1c0a-4b35-834a-b42e6baf2816&gid=3');
-		$this->assertLocation(42.618700, -82.477967, 'https://www.waymarking.com/gallery/image.aspx?f=1&guid=e1afc7aa-1dea-4110-9ae9-4d2b2c6deb68');
-	}
-
-	/**
-	 * @group request
-	 */
-	public function testGallery(): void
-	{
-		$this->assertLocation(49.198050, 16.607533, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=9cc31753-8cfb-4b44-9c57-c2b303fd1a9b&gid=2');
-		$this->assertLocation(42.618700, -82.477967, 'https://www.waymarking.com/gallery/default.aspx?f=1&guid=01645b8d-404c-41f1-a951-190032f55215&gid=2');
+		$service = new WaymarkingService();
+		$this->assertServiceLocation($service, $input, $expectedLat, $expectedLon);
 	}
 }
