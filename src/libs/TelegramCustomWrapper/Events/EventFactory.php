ded5dcc81b7631451fdae6d030fe7e79fe8877e7
Tomas Palider (DJTommek) <tomas@palider.cz>
2024-09-28 09:22:24 +0200

    EventFactory: greatly optimized by loading event instance on demand instead of when factory initialization

--- src/libs/TelegramCustomWrapper/Events/EventFactory.php
+++ src/libs/TelegramCustomWrapper/Events/EventFactory.php
@@ -11,17 +11,16 @@ class EventFactory
 	use ObjectsFilterTrait;
 
 	/**
-	 * @var array<class-string,Events>
+	 * @var array<class-string<Events>,Events>
 	 */
-	private readonly array $events;
+	private array $eventsArray = [];
 
 	/**
 	 * @param iterable<object> $events
 	 */
-	public function __construct(iterable $events)
-	{
-		// @TODO {rqd9s3z9i9} Filter can be removed once linked TODO is resolved
-		$this->events = iterator_to_array($this->filter($events, Events::class, true));
+	public function __construct(
+		private readonly iterable $events,
+	) {
 	}
 
 	/**
@@ -29,7 +28,13 @@ class EventFactory
 	 */
 	public function create(string $event, Update $update): Events
 	{
-		$eventInstance = $this->events[$event];
+		if ($this->eventsArray === []) {
+			// @TODO {rqd9s3z9i9} Filter can be removed once linked TODO is resolved
+			$filteredEvents = $this->filter($this->events, Events::class, true);
+			$this->eventsArray = iterator_to_array($filteredEvents);
+		}
+
+		$eventInstance = $this->eventsArray[$event];
 		assert($eventInstance instanceof Events);
 		$eventInstance->setUpdateObject($update);
 		return $eventInstance;
